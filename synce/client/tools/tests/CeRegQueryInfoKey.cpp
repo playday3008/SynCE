cdd20c1d1cc06d99927f1ad18e10a7ea4b3b9594
David Eriksson <david@activout.se>
2001-10-17 00:36:46 +0000

    improved test
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@34 599ab11a-0111-0410-8a50-d7a5b0953570

--- synce/client/tools/tests/CeRegQueryInfoKey.cpp
+++ synce/client/tools/tests/CeRegQueryInfoKey.cpp
@@ -4,6 +4,12 @@ int main()
 {
 	VERIFY_HRESULT(CeRapiInit());
 
+	HKEY hkey;
+
+	// This test will fail without this subkey... :-)
+	WCHAR * subkey = to_unicode("Comm");
+	VERIFY_EQUAL(ERROR_SUCCESS, CeRegOpenKeyEx(HKEY_LOCAL_MACHINE, subkey, 0, 0, &hkey));
+
 	WCHAR classname[MAX_PATH];
 	DWORD classname_size = MAX_PATH;
 	DWORD subkey_count = 0;
@@ -14,9 +20,9 @@ int main()
 	DWORD max_value_data_size = 0;
 	FILETIME filetime;
 
-	// XXX: wrong return value
-	// XXX: only subkey_count parameter implemented
-	TEST_EQUAL(ERROR_SUCCESS, CeRegQueryInfoKey(HKEY_CURRENT_USER, classname,
+	classname[0] = 0;
+	
+	TEST_EQUAL(ERROR_SUCCESS, CeRegQueryInfoKey(hkey, classname,
 				&classname_size, NULL, &subkey_count, &max_subkey_name_size, 
 				&max_subkey_class_size, &value_count, &max_value_name_size,
 			  &max_value_data_size, NULL, &filetime	));
@@ -28,6 +34,7 @@ int main()
 			max_subkey_class_size, value_count, max_value_name_size,
 			max_value_data_size);
 
+	VERIFY_EQUAL(ERROR_SUCCESS, CeRegCloseKey(hkey));
 	VERIFY_HRESULT(CeRapiUninit());
 	return TEST_SUCCEEDED;
 }
