7e1ba056b1bd6a8de2b46c84485286ef814b5e28
David Eriksson <david@activout.se>
2001-10-23 20:18:10 +0000

    Added tiny documentation and licencing
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@69 599ab11a-0111-0410-8a50-d7a5b0953570

--- kio_synce/synce/synce.h
+++ kio_synce/synce/synce.h
@@ -1,3 +1,21 @@
+/*
+    kio_synce - an ioslave for KDE2
+    Copyright (C) 2001  David Eriksson
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #ifndef __synce_h__
 #define __synce_h__
 
@@ -9,6 +27,10 @@
 #include <kio/global.h>
 #include <kio/slavebase.h>
 
+//
+// Use a special namespace for this, because of name clashes
+// (for example HANDLE)
+//
 namespace RAPI
 {
 extern "C"
