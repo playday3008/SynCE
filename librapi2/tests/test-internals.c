c69f32bf30c17916c195a44019b3063d0c807e24
David Eriksson <david@activout.se>
2002-07-22 08:00:31 +0000

    Simple tests for reading and writing to buffer
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@131 599ab11a-0111-0410-8a50-d7a5b0953570

--- librapi2/tests/test-internals.c
+++ librapi2/tests/test-internals.c
@@ -4,10 +4,52 @@
 
 #include "rapi_buffer.h"
 
+#define VALUE_16BIT 0x1234
+#define VALUE_32BIT 0x6789abcd
 
-START_TEST(test_buffer)
+START_TEST(test_buffer_uint32)
 {
+	bool success;
+	RapiBuffer* buffer;
+	u_int32_t value;
 
+	buffer = rapi_buffer_new();
+	fail_unless(buffer != NULL, "rapi_buffer_new");
+	
+	value = VALUE_32BIT;
+	success = rapi_buffer_write_uint32(buffer, value);
+	fail_unless(success, "rapi_buffer_write_uint32");
+	
+	value = 0;
+	success = rapi_buffer_read_uint32(buffer, &value);
+	fail_unless(success, "rapi_buffer_write_uint32");
+
+	fail_unless(VALUE_32BIT == value, "Write/read sequence failed");
+	
+	rapi_buffer_free(buffer);
+}
+END_TEST
+
+START_TEST(test_buffer_uint16)
+{
+	bool success;
+	RapiBuffer* buffer;
+	u_int16_t value;
+
+	buffer = rapi_buffer_new();
+	fail_unless(buffer != NULL, "rapi_buffer_new");
+	
+	value = VALUE_16BIT;
+	success = rapi_buffer_write_uint16(buffer, value);
+	fail_unless(success, "rapi_buffer_write_uint16");
+	
+	value = 0;
+	success = rapi_buffer_read_uint16(buffer, &value);
+	fail_unless(success, "rapi_buffer_read_uint16");
+
+	fail_unless(VALUE_16BIT == value, "Write/read sequence failed");
+	
+	rapi_buffer_free(buffer);
 }
 END_TEST
 
@@ -18,7 +60,8 @@ Suite *internals_suite (void)
 	TCase *tc_buffer = tcase_create ("rapi_buffer");
 
 	suite_add_tcase (s, tc_buffer);
-	tcase_add_test (tc_buffer, test_buffer); 
+	tcase_add_test (tc_buffer, test_buffer_uint32); 
+	tcase_add_test (tc_buffer, test_buffer_uint16); 
 
 	return s; 
 }
