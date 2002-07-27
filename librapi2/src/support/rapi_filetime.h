10215a482ab39d526ed526c062b58614d11fc0ec
David Eriksson <david@activout.se>
2002-07-27 16:30:08 +0000

    support c++
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@210 599ab11a-0111-0410-8a50-d7a5b0953570

--- librapi2/src/support/rapi_filetime.h
+++ librapi2/src/support/rapi_filetime.h
@@ -6,8 +6,17 @@
 
 #include <time.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 void rapi_filetime_from_unix_time(time_t unix_time, FILETIME *filetime);
 time_t rapi_filetime_to_unix_time(const FILETIME *filetime);
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif 
