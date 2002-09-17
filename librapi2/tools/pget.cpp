c70af727791430658690e371b8f3aacae69f8c75
David Eriksson <david@activout.se>
2002-09-17 12:28:21 +0000

    - Renamed pcp to pput - Added pget contributed by Richard Taylor
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@239 599ab11a-0111-0410-8a50-d7a5b0953570

--- librapi2/tools/pget.cpp
+++ librapi2/tools/pget.cpp
@@ -1,7 +1,11 @@
 //
-// pcp - PocketCoPy
+// pget - PocketGET
 //
-// Copies a file to a Pocket PC handheld
+// Copies a file from a Pocket PC handheld
+//
+// Modified version of pput.
+//
+// Contributed by Richard Taylor <synce@rest.clara.co.uk>
 //
 #include <malloc.h>
 #include <libgen.h>
@@ -18,7 +22,7 @@
 
 //
 // HRESULT tests
-// 
+//
 #define VERIFY_HRESULT(call) \
 if (FAILED((call))) { printf("FAIL.\n"); return TEST_FAILED; }
 
@@ -58,48 +62,50 @@ VERIFY_NOT_EQUAL((value), (call)) else printf("ok.\n");
 int main(int argc, char**argv)
 {
 	if (2 != argc)
-	{	
+	{
 		fprintf(stderr, "Syntax:\n"
 				"\t%s source-file\n"
 				"\n"
-				"Copy source-file to the \"My Documents\" directory on the Pocket PC\n", 
+				"Copy source-file from the \"My Documents\" directory on the Pocket PC\n",
 				argv[0]);
 		return 1;
 	}
 
 	char* source_file = strdup(argv[1]);
-	
+
 	VERIFY_HRESULT(CeRapiInit());
 
 	DWORD length;
-	WCHAR dest_file[MAX_PATH];
-	VERIFY_NOT_FALSE(length = CeGetSpecialFolderPath(CSIDL_PERSONAL, MAX_PATH, dest_file));
-	rapi_wstr_append(dest_file, rapi_wstr_from_ascii("\\"), sizeof(dest_file));
-	rapi_wstr_append(dest_file, rapi_wstr_from_ascii(basename(source_file)), sizeof(dest_file));
-	printf("Remote filename: \"%s\"\n", rapi_wstr_to_ascii(dest_file));
-	
-	FILE* source = fopen(source_file, "r");
-	if (!source)
+	WCHAR source_path[MAX_PATH];
+	VERIFY_NOT_FALSE(length = CeGetSpecialFolderPath(CSIDL_PERSONAL, MAX_PATH, source_path));
+	rapi_wstr_append(source_path, rapi_wstr_from_ascii("\\"), sizeof(source_path));
+	rapi_wstr_append(source_path, rapi_wstr_from_ascii(basename(source_file)), sizeof(source_path));
+	printf("Remote filename: \"%s\"\n", rapi_wstr_to_ascii(source_path));
+
+	FILE* dest = fopen(source_file, "w");
+	if (!dest)
 	{
 		return 1;
 	}
-			
+
 	free(source_file);
 
 	HANDLE handle;
 
 	// Open file for writing, create if it does not exist
-	TEST_NOT_EQUAL(INVALID_HANDLE_VALUE, handle = CeCreateFile(dest_file,
-				GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0));
-	
+	TEST_NOT_EQUAL(INVALID_HANDLE_VALUE, handle = CeCreateFile(source_path,
+				GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0));
+
 #define MYBUF_SIZE (16*1024)
 	char * buffer = new char[MYBUF_SIZE];
-	
+
 	size_t bytes_read;
-	while (0 != (bytes_read = fread(buffer, 1, MYBUF_SIZE, source)))
+	while (1)
 	{
-		DWORD did_write = (DWORD)-1;
-		TEST_NOT_FALSE(CeWriteFile(handle, buffer, bytes_read, &did_write, NULL));
+		TEST_NOT_FALSE(CeReadFile(handle, buffer, MYBUF_SIZE, &bytes_read, NULL));
+		if (bytes_read == 0) break;
+		size_t did_write =(DWORD)-1;
+		did_write = fwrite(buffer, 1, bytes_read, dest);
 		if (bytes_read != did_write)
 		{
 			printf("Only wrote %u bytes to file of %u possible.\n", did_write, bytes_read);
@@ -111,7 +117,7 @@ int main(int argc, char**argv)
 	delete buffer;
 
 	// Close files
-	fclose(source);
+	fclose(dest);
 	TEST_NOT_FALSE(CeCloseHandle(handle));
 
 	VERIFY_HRESULT(CeRapiUninit());
