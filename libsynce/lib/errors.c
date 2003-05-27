4fb01681453c07be7a920845c13d3846e58adeef
David Eriksson <david@activout.se>
2003-05-27 06:49:53 +0000

    More error messages.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@834 599ab11a-0111-0410-8a50-d7a5b0953570

--- libsynce/lib/errors.c
+++ libsynce/lib/errors.c
@@ -9,12 +9,23 @@ char* synce_strerror(DWORD error)
 	switch (error)
 	{
 		case S_OK: 			result = "Success";   break;
-		case S_FALSE: 	result = "False"; 		break;
-		case E_FAIL: 		result = "Failure"; 	break;
+		case S_FALSE: 	result = "FALSE"; 		break;
 
-		case ERROR_FILE_NOT_FOUND: result = "File not found"; break;
-		case ERROR_PATH_NOT_FOUND: result = "Path not found"; break;
-		case ERROR_DIR_NOT_EMPTY:  result = "Directory not empty"; break;
+    case E_ABORT:         result = "The operation was aborted because of an unspecified error"; break;
+    case E_ACCESSDENIED:  result = "A general access-denied error"; break;
+		case E_FAIL: 		      result = "An unspecified failure has occurred"; 	break;
+    case E_HANDLE:        result = "An invalid handle was used"; break;
+    case E_INVALIDARG:  	result = "One or more arguments are invalid."; break;
+
+    case E_NOTIMPL:       result = "The method is not implemented"; break;
+    case E_OUTOFMEMORY:   result = "The method failed to allocate necessary memory"; break;
+    case E_PENDING:       result = "The data necessary to complete the operation is not yet available"; break;
+    case E_POINTER:       result = "An invalid pointer was used"; break;
+    case E_UNEXPECTED:    result = "A catastrophic failure has occurred"; break;
+
+		case ERROR_FILE_NOT_FOUND:    result = "File not found"; break;
+		case ERROR_PATH_NOT_FOUND:    result = "Path not found"; break;
+		case ERROR_DIR_NOT_EMPTY:     result = "Directory not empty"; break;
 		case ERROR_INVALID_PARAMETER: result = "Invalid parameter"; break;
 		
 		default: 
