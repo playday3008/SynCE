31dd53f10ef521ff818d1b928e29140c473d07e6
David Eriksson <david@activout.se>
2003-06-01 14:01:12 +0000

    Added some more info
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@878 599ab11a-0111-0410-8a50-d7a5b0953570

--- htdocs/multisync.php
+++ htdocs/multisync.php
@@ -12,6 +12,9 @@
 href="http://www.ximian.com/products/evolution/">Ximian Evolution</a> using a
 plugin for <a href="http://multisync.sourceforge.net/">MultiSync</a>.</p>
 
+<p>The <a href="http://sourceforge.net/cvs/?group_id=30550">CVS version</a> of
+SynCE can also synchronize appointments and tasks.</p>
+
 <p><b>Warning:</b> This is experimental software. Anything can happen. Use it
 at your own risk.</p>
 
@@ -34,7 +37,8 @@ Pocket PC handheld, follow the instructions at <i>Using both SynCE and
 ActiveSync</i> below to change the current partnership.</li>
 
 <li class=SPACED>Create a MultiSync synchronization pair with Evolution and the
-SynCE plugin.</li>
+SynCE plugin. You should only have the SynCE plugin in <i>one</i> connection
+pair.</li>
 
 <li class=SPACED>Synchronize!</li>
 
