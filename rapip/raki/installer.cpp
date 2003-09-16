2ae9a79ac184859e97bb1a8cfd9b5dbd9f3aebe9
Volker Christian <me@vchrist.at>
2003-09-16 17:53:58 +0000

    The \Windows\AppMgr\Install directory is checked for existence and created if necessary.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1291 599ab11a-0111-0410-8a50-d7a5b0953570

--- rapip/raki/installer.cpp
+++ rapip/raki/installer.cpp
@@ -29,6 +29,8 @@
 #include <qstringlist.h>
 #include <kapplication.h>
 #include <kcombobox.h>
+#include <kio/netaccess.h>
+#include <kdebug.h>
 
 #ifdef WITH_DMALLOC
 #include <dmalloc.h>
@@ -36,7 +38,7 @@
 #endif
 
 Installer::Installer(QWidget *parent, QDict<PDA> *pdaList)
-  : InstallDialog(parent)
+        : InstallDialog(parent)
 {
     this->pdaList = pdaList;
     runInstallerThread = new RunInstallerThread(parent);
@@ -52,7 +54,7 @@ Installer::~Installer()
 void Installer::runInstaller(KURL destUrl)
 {
     PDA *pda = (PDA *) pdaList->find(destUrl.host());
-    
+
     if (pda != NULL) {
         if (pda->currentInstalled) {
             pda->currentInstalled = 0;
@@ -69,11 +71,11 @@ void Installer::deleteResult(KIO::Job *deleteJob)
     if (deleteJob->error()) {
         deleteJob->showErrorDialog((QWidget *) parent());
     }
-    
+
     KURL::List destUrls = ((KIO::DeleteJob *) deleteJob)->urls();
-    
+
     PDA *pda = (PDA *) pdaList->find(destUrls.first().host());
-    
+
     if (pda != NULL) {
         pda->installed++;
         if (pda->installed == pda->installCounter) {
@@ -94,12 +96,12 @@ void Installer::deleteFiles(KURL::List& delFiles)
 void Installer::copyResult(KIO::Job *copyJob)
 {
     KURL destUrl = ((KIO::CopyJob *) copyJob)->destURL();
-    
+
     PDA *pda = (PDA *) pdaList->find(destUrl.host());
-    
+
     if (pda != NULL) {
         KURL::List& copiedFiles = pda->getURLListByCopyJob(
-                                          (KIO::CopyJob *) copyJob);
+                                      (KIO::CopyJob *) copyJob);
         if (copyJob->error()) {
             copyJob->showErrorDialog((QWidget *) parent());
             sleep(1);
@@ -121,11 +123,12 @@ void Installer::procFiles(KIO::Job *job, const KURL& from, const KURL& to)
     QString pdaName = to.host();
     KURL dest;
     PDA *pda = (PDA *) pdaList->find(pdaName);
-    
+
+    disconnect(job, SIGNAL(copying(KIO::Job *, const KURL&, const KURL&)), 0, 0);
+
     if (pda != NULL) {
-        dest = KURL("rapip://" + pdaName + 
-                "/Windows/AppMgr/Install/" + from.fileName());
-    
+        pda->installCounter++;
+        dest = KURL("rapip://" + pdaName + "/Windows/AppMgr/Install/" + from.fileName());
         pda->addURLByCopyJob((KIO::CopyJob *) job, dest);
     }
 }
@@ -135,7 +138,7 @@ void Installer::install()
 {
     KURL::List ul;
     QStringList::Iterator slit;
-    
+
     for (slit = installFiles.begin(); slit != installFiles.end(); ++slit) {
         if ((*slit).lower().endsWith(".cab")) {
             ul.append(*slit);
@@ -143,20 +146,32 @@ void Installer::install()
     }
 
     QString pdaName = pdas->currentText();
-    
+
     PDA *pda = (PDA *) pdaList->find(pdaName);
-    
+
     if (pda != NULL && !ul.empty()) {
-        pda->installCounter++;
-        KIO::CopyJob *copyJob = KIO::copy(ul, KURL("rapip://" + pdaName + 
-                "/Windows/AppMgr/Install/"), true);       
-        connect(copyJob, SIGNAL(result(KIO::Job *)), this, 
-                SLOT(copyResult(KIO::Job *)));
-        connect(copyJob, SIGNAL(copying(KIO::Job *, const KURL&, const KURL&)),
-                this, SLOT(procFiles (KIO::Job *, const KURL&, const KURL&)));
-        pda->registerCopyJob(copyJob);
+        bool mkdirSuccess = true;
+
+        if (!KIO::NetAccess::exists("rapip://" + pdaName + "/Windows/AppMgr/Install")) {
+            if (!KIO::NetAccess::exists("rapip://" + pdaName + "/Windows/AppMgr")) {
+                mkdirSuccess = KIO::NetAccess::mkdir("rapip://" + pdaName + "/Windows/AppMgr");
+            }
+            if (mkdirSuccess) {
+                mkdirSuccess = KIO::NetAccess::mkdir("rapip://" + pdaName + "/Windows/AppMgr/Install");
+            }
+        }
+
+        if (mkdirSuccess) {
+            KIO::CopyJob *copyJob = KIO::copy(ul, KURL("rapip://" + pdaName +
+                    "/Windows/AppMgr/Install/"), true);
+            connect(copyJob, SIGNAL(result(KIO::Job *)), this,
+                    SLOT(copyResult(KIO::Job *)));
+            connect(copyJob, SIGNAL(copying(KIO::Job *, const KURL&, const KURL&)),
+                    this, SLOT(procFiles (KIO::Job *, const KURL&, const KURL&)));
+            pda->registerCopyJob(copyJob);
+        }
     }
-    
+
     close();
 }
 
@@ -165,13 +180,13 @@ void Installer::show(QStringList installFiles)
 {
     pdas->clear();
     this->installFiles = installFiles;
-    
-    QDictIterator<PDA> it(*pdaList);  
-    
+
+    QDictIterator<PDA> it(*pdaList);
+
     for (; it.current(); ++it ) {
         pdas->insertItem((*it)->getName());
     }
-    
+
     InstallDialog::show();
 }
 
@@ -198,8 +213,7 @@ void RunInstallerThread::work(QThread */*th*/, void */*data*/)
 
     if (Ce::rapiInit(pdaName)) {
         if (!Ce::createProcess(QString("wceload.exe").ucs2(), NULL,
-                               NULL, NULL, false, 0, NULL, NULL, NULL, &info)) {
-        }
+                           NULL, NULL, false, 0, NULL, NULL, NULL, &info)) {}
         Ce::rapiUninit();
     }
 }
