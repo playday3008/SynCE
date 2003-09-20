d562e57dfd7b084c369a32b335621a88ef19fa3e
Volker Christian <me@vchrist.at>
2003-09-20 00:13:05 +0000

    Fix
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1310 599ab11a-0111-0410-8a50-d7a5b0953570

--- htdocs/kde/screenshots.php
+++ htdocs/kde/screenshots.php
@@ -1,20 +1,5 @@
 <?php include 'header.php'; ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta name="generator" content=
-"HTML Tidy for Linux/x86 (vers 1st June 2003), see www.w3.org" />
-<meta http-equiv="Content-Type" content=
-"text/html; charset=us-ascii" />
-<title>SynCE-KDE</title>
-<link href="synce.css" rel="stylesheet" type="text/css" />
-<meta name="author" content="Volker Christian" />
-</head>
-<body>
-<div class="PAGE">
-<div class="PAGE">
 <p>Return to <a href="index.php">SynCE-KDE main page</a>.</p>
 <h1>SynCE - KDE Screenshots</h1>
 <br />
