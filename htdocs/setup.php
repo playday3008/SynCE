c13eb379ddfed7887d61a52165af29b5eb509b02
David Eriksson <david@activout.se>
2003-10-28 20:14:17 +0000

    Get rid of using.php
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1386 599ab11a-0111-0410-8a50-d7a5b0953570

--- htdocs/setup.php
+++ htdocs/setup.php
@@ -6,6 +6,22 @@
 
 <h1>SynCE - Setup</h1>
 
+<h2>Open firewall</h2>
+
+<p>You need to allow your handheld device to access ports 5678 and 5679 on your
+PC. The PC will need to access port 990 on your handheld device.</p>
+
+<p>If you are running Linux, you can list your firewall rules with <tt>ipchains
+-L</tt> for Linux kernel 2.2 and <tt>iptables -L</tt> for Linux kernel 2.4.</p>
+
+<p>If you are running RedHat Linux, you may have RedHat's built-in firewall
+function enabled. Run either the <tt>lokkit</tt> or the
+<tt>redhat-config-securitylevel</tt> program as root to open the ports listed
+above.</p>
+
+
+<h2>Connection</h2>
+
 <p>Please select the type of connection you will use:</p>
 
 <ul>
