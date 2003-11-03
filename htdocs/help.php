5d340c258f7d025b810e1b698bc9295d46bdfe87
David Eriksson <david@activout.se>
2003-11-03 05:58:24 +0000

    Some things that might be useful to include when you ask for help.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1389 599ab11a-0111-0410-8a50-d7a5b0953570

--- htdocs/help.php
+++ htdocs/help.php
@@ -16,19 +16,31 @@ questions</a></li>
 
 <li class=SPACED>Read the <a href="qa.php">Questions &amp; answers</a> page</li>
 
-<li class=SPACED>Search the <a href="http://sourceforge.net/mail/?group_id=30550">mailing
+<li>Search the <a href="http://sourceforge.net/mail/?group_id=30550">mailing
 list archives</a> and <a
 href="http://sourceforge.net/projects/synce/">forums</a> to see if your
 problem has been discussed before</li>
 
-<li class=SPACED>If you still can't solve the problem send mail to a suitable <a
+<li><p>If you still can't solve the problem send mail to a suitable <a
 href="http://sourceforge.net/mail/?group_id=30550">mailing list</a> or write a
 new entry in one of the <a
-href="http://sourceforge.net/projects/synce/">forums</a></li>
+href="http://sourceforge.net/projects/synce/">forums</a>. These are some things
+that might be useful to include when you ask for help:</p>
+
+<ul>
+
+<li>Device name and Windows CE version</li>
+<li>Connection method (serial cable, USB cable, IrDA, Bluetooth)</li>
+<li>Operating system name, distribution and kernel version</li>
+<li>SynCE, MultiSync and Evolution versions</li>
+<li>If you compiled SynCE yourself or used pre-compiled packages</li>
+<li>Relevant log extracts from kernel driver and pppd</li>
+<li>Your <tt>/etc/ppp/peers/synce-device</tt> file (created by synce-serial-config)</li>
+<li>Output from <tt>dccm -f -d 3</tt> from when your PDA connects</li>
+
+</ul></li>
 
 </ol>
-
-
 
 <p>Return to <a href="index.php">main page</a>.</p>
 
