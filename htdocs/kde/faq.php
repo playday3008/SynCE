7a4715f620d74c63fe336c87b8ba0b464000e153
Volker Christian <me@vchrist.at>
2003-11-15 18:35:31 +0000

    Added a faq addressing the inactive "Synchronization" menu entry
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1413 599ab11a-0111-0410-8a50-d7a5b0953570

--- htdocs/kde/faq.php
+++ htdocs/kde/faq.php
@@ -3,7 +3,7 @@
 <p>Return to <a href="index.php">SynCE-KDE main page</a>.</p>
 <h1>SynCE -
 KDE Frequently Asked Questions</h1>
-<h2>Contents</h2>
+<h2><a class="mozTocH2" name="mozTocId84199"></a>Contents</h2>
 <ul id="mozToc">
 <!--mozToc h2 1 h3 2 h4 3 h5 4 h6 5 h6 6--><li><a href="#mozTocId81386">Compilation
 and
@@ -17,9 +17,9 @@ Linux distribution xy. </a></li>
   </li>
   <li><a href="#mozTocId363079">Startup</a>
     <ul>
-      <li><a href="#mozTocId679954">Q21: I have RAKI up and running but
-when I connect my PDA to the RS-232/USB/irDA port it didn't connect
-automatically.</a></li>
+      <li><a href="#mozTocId679954">Q21: I have RAKI up
+and running but when I connect my PDA to the RS-232/USB/irDA port it
+didn't connect automatically.</a></li>
       <li><a href="#mozTocId147393">Q22: I have RAKI up
 and running but when I connect my PDA, RAKI didn't honor the connection
 and the icon didn't turn colorful.</a></li>
@@ -27,9 +27,14 @@ and the icon didn't turn colorful.</a></li>
   </li>
   <li><a href="#mozTocId293095">Handling</a>
     <ul>
-      <li><a href="#mozTocId451238">Q31: When I initiate a connection
-to my PDA RAKI brings up the initialization progress bar but blocks at
-a progress of 33%. I have only the possibility to kill raki.</a></li>
+      <li><a href="#mozTocId451238">Q31: When I initiate
+a connection to my PDA RAKI brings up the initialization progress bar
+but blocks at a progress of 33%. I only have the possibility to kill
+it.</a></li>
+      <li><a href="#mozTocId95584">Q32: My PDA has successful
+connected to RAKI but the menu entry Synchronize is inactive and grayed
+out and no Synchronization Types are present in the configuration
+dialog.</a></li>
     </ul>
   </li>
 </ul>
@@ -42,7 +47,8 @@ install</tt> I can't find the RAKI icon in my K-Menu hierarchy. I use
 Linux distribution xy. </h3>
 <blockquote><b>A11:</b> You haven't configured your SynCE-KDE package
 the right way. Every Linux distribution uses its own directory
-structure and <tt>./configure </tt>couldn't find out the right one. So
+structure and <tt>./configure </tt>couldn't find out the right one.
+So
 you have to help a little.<br>
   <ol>
     <li>Find out the base directory of your KDE installation. An easy
@@ -59,10 +65,10 @@ files. Every <tt>*.desktop</tt> file appears in the K-Menu as a menu
 item. To find out this directory<br>
       <blockquote>search for the file <tt>konqbrowser.desktop</tt><br>
       </blockquote>
-in your filesystem. Most likely it is in a subdirectory of <tt>$KDEDIR</tt>
+in your file system. Most likely it is in a subdirectory of <tt>$KDEDIR</tt>
 namely <tt>$KDEDIR/share/applnk/Internet/konqbrowser.desktop</tt>.
-The path to <tt>konqbrowser.desktop</tt> contains the relevant path
-$kde_appsdir. It is the leading part of the path to <tt>konqbrowser.desktop</tt>
+The path to <tt>konqbrowser.desktop</tt> contains the relevant path <tt>$kde_appsdir</tt>.
+It is the leading part of the path to <tt>konqbrowser.desktop</tt>
 and follows the scheme<br>
       <blockquote><tt>$kde_appsdir/Internet/konqbrowser.desktop</tt></blockquote>
     </li>
@@ -103,11 +109,12 @@ and the icon didn't turn colorful.</h3>
     <li>You have chosen a specific DCCM at the first start of RAKI but
 you have started the other DCCM by hand before you start RAKI. <br>
       <b>Solution:</b> Kill the DCCM you have started by hand and let
-RAKI start the right DCCM itself.</li><br>
+RAKI start the right DCCM itself.</li>
+    <br>
     <li>You have chosen a specific DCCM at the first start of RAKI but
 the connection management script <tt>~/.synce/scripts/dccm.sh</tt> is
 the one designed for the other DCCM.<br>
-<b>Solution:</b> Stop RAKI, open the file <tt>~/.kde/share/config/rakirc</tt>
+      <b>Solution:</b> Stop RAKI, open the file <tt>~/.kde/share/config/rakirc</tt>
 with an
 editor and remove the line <tt>VERSION=x.y.z</tt> in the block <tt>[RAKI]</tt>
 and start
@@ -125,7 +132,44 @@ it.</h3>
 <blockquote><b>A31:</b> This is a bug in SynCE-KDE-0.6 and before. It
 is fixed since SynCE-KDE-0.6.1.<br>
 </blockquote>
-<br>
+<h3><a class="mozTocH3" name="mozTocId95584"></a>Q32: My PDA has
+successful connected to RAKI but the menu entry <i>Synchronize</i> is
+inactive and grayed out and no Synchronization Types are present in the
+configuration dialog.</h3>
+<blockquote><b>A32:</b> There is no partnership created between RAKI
+and your PDA.&nbsp; This problem could have two reasons:<br>
+  <ol>
+    <li>You have already two partnerships active on your PDA and you
+have not removed one of them during the connection process of your PDA
+and RAKI. <b>Solution:</b> Disconnect your PDA and reconnect it again.
+When the dialog box <i>Remove Partnership</i> appears, select one (or
+both) of the <i>Current Partnerships</i> and click <i>Remove marked
+Partnerships</i> and than click <i>Ok</i>. The selected partnerships
+are removed on your PDA and a new one between RAKI and your PDA is
+created. The <i>Synchronize</i> menu entry now should be active.<br>
+    </li>
+    <li>You are using the original DCCM part of the SynCE and not VDCCM
+part of SynCE-KDE. A partnership between your PDA and RAKI could only
+be created if you are using VCDDM. <br>
+      <b>Solution:</b> To switch to VDCCM do the following:<br>
+      <ol>
+        <li>Disconnect your PDA and quit RAKI.<br>
+        </li>
+        <li>Open the file <tt>~/.kde/share/config/rakirc</tt> in an
+editor and remove the lines<br>
+          <tt>------------------------------<br>
+[RAKI]<br>
+Version=x.y.z<br>
+------------------------------</tt><br>
+from it. Save the file again.</li>
+        <li>Start RAKI again - the splash screen should appear again.</li>
+        <li>Select VDCCM on the splash screen and press <i>Ok</i>.</li>
+        <li>Connect your PDA again.<br>
+        </li>
+      </ol>
+    </li>
+  </ol>
+</blockquote>
 <p>Return to <a href="index.php">SynCE-KDE main page</a>.</p>
 
 <?php include 'footer.php'; ?>
