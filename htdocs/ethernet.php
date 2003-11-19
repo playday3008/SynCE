a3f30006141dace152b2d0e36a0365c2e3b78a25
David Eriksson <david@activout.se>
2003-11-19 10:20:10 +0000

    Minor clean-up.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1421 599ab11a-0111-0410-8a50-d7a5b0953570

--- htdocs/ethernet.php
+++ htdocs/ethernet.php
@@ -6,25 +6,27 @@
 
 <h1>SynCE - Ethernet/WLAN connection</h1>
 
-In this documentation it is assumed that you have already successfull 
-installed your Ethernet/WLAN card in your device. We could not provide
-support for this step as the installation procedure differs between different
-network cards.
+<p>In this documentation it is assumed that you have already successfull installed
+your Ethernet/WLAN card in your device. We could not provide support for this
+step as the installation procedure differs between different network cards.</p>
 
 <ol>
 
-<li class=SPACED>Before you could start using your Ethernet/WLAN connection with
-SynCE you have to connect your PDA via one of the serial-based connection methodes:
+<li class=SPACED><p>Before you could start using your Ethernet/WLAN connection
+with SynCE you have to connect your PDA via one of the serial-based connection
+methods:</p>
+
 <ul>
 <li><a href="serial.php">Serial cable</a></li>
 <li><a href="infrared.php">Infrared</a></li>
 <li><a href="usb.php">USB cable</a> or</li>
 <li><a href="bluetooth.php">Bluetooth</a></li>
 </ul>
-Before doing this make sure that the hostname of your desktop is known by a name server.
+
+<p>Before doing this make sure that the hostname of your desktop is known by a name server.
 This is neccessary because the PDA does hostname resolution to initiate a Ethernet/WLAN 
 connection. This hostname is stored in the registry of your PDA during the initial 
-connection sequence.</li>
+connection sequence.</p></li>
 
 <li class=SPACED>Connect your device to the Ethernet/WLAN and make sure there is a name server
 specified in the network settings of it.</li>
@@ -32,16 +34,16 @@ specified in the network settings of it.</li>
 <li class=SPACED>Open ActiveSync on your device and click on <tt>Extras</tt> in the lower left
 corner.</li>
 
-<li class=SPACED>Choose the <tt>Options...</tt> menu entry and make sure, the checkbox
-before "PC bei Remotesynchronisierung einschliessen und verbinden mit:" (Sorry, I only
-have devices localized for german).</li>
+<li class=SPACED>Choose the <tt>Options...</tt> menu entry and make sure, the
+checkbox before "Include PC when synchronizing remotely and connect to". (German
+version: "PC bei Remotesynchronisierung einschliessen und verbinden mit".)</li>
 
 <li class=SPACED>Choose your PC in the combobox below the checkbox and acknowledge with
 <tt>ok</tt>.</li>
 
 <li class=SPACED>Make sure dccm is running on your desktop.</li>
 
-<li class=SPACED>On the main screen of ActiveSync on your PDA click <tt>Synchronisieren</tt></li>
+<li class=SPACED>On the main screen of ActiveSync on your PDA click <tt>Sync</tt>. (German version: <tt>Synchronisieren</tt>.)</li>
 
 </ol>
 
