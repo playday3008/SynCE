965d675a99810ad1f48a2e92cd47b174516ca2ab
David Eriksson <david@activout.se>
2004-03-04 20:41:10 +0000

    Vector of mdir_line pointers, using libsynce/lib/synce_vector_template.h
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1573 599ab11a-0111-0410-8a50-d7a5b0953570

---
+++ rra/lib/mdir_line_vector.c
@@ -0,0 +1,5 @@
+/* $Id$ */
+#include "mdir_line_vector.h"
+
+SYNCE_VECTOR_IMPLEMENT(RRA_MdirLineVector, rra_mdir_line_vector, mdir_line*)
+
---
+++ rra/lib/mdir_line_vector.h
@@ -0,0 +1,10 @@
+/* $Id$ */
+#ifndef __mdir_line_vector_h__
+#define __mdir_line_vector_h__
+
+#include <libmimedir.h>
+#include <synce_vector_template.h>
+
+SYNCE_VECTOR_DECLARE(RRA_MdirLineVector, rra_mdir_line_vector, mdir_line*)
+
+#endif
