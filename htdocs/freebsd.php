42372da9c8de2a1f8748c636f8ab43a6a879c89f
David Eriksson <david@activout.se>
2004-03-26 07:16:14 +0000

    New address.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1601 599ab11a-0111-0410-8a50-d7a5b0953570

--- htdocs/freebsd.php
+++ htdocs/freebsd.php
@@ -6,8 +6,8 @@
 
 <h1>SynCE - FreeBSD support</h1>
 
-<p>Visit <a href="http://www.brooknet.com.au/~boris">SynCE on FreeBSD</a> by
-Sam Lawrance.</p>
+<p>Visit <a href="http://sam.stral.net/freebsd/synce.html">SynCE on FreeBSD</a>
+by Sam Lawrance.</p>
 
 </div>
 <?php include 'footer.php'; ?>
