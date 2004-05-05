72f748d67d47fe6c98d595753e31a40ee60f232f
David Eriksson <david@activout.se>
2004-05-05 19:55:39 +0000

    - Improved alignment - Can skip past drawings now
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1679 599ab11a-0111-0410-8a50-d7a5b0953570

--- twogood-files/pwi/pwi.cc
+++ twogood-files/pwi/pwi.cc
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <stdint.h>
 
+#define VERBOSE 0
+
 using namespace std;
 
 #define PWI_MAGIC         "{\\pwi"
@@ -51,6 +53,147 @@ static unsigned read16(istream& input)
   return value;
 }
 
+void text(istream& input)/*{{{*/
+{
+  input.seekg(0x4, ios_base::cur);
+
+  /*unsigned plain_text_size      =*/ read16(input);
+  unsigned total_size           = read16(input);
+
+  //cerr << "Total size: 0x" << hex << total_size << endl;
+
+  input.seekg(0x14, ios_base::cur);
+
+  uint8_t* data = new uint8_t[total_size];
+
+  unsigned paragraph_data_offset = input.tellg();
+  input.read((char*)data, total_size);
+
+  for (unsigned j = 0; j < total_size; j++)
+  {
+    if (data[j] == 0x00)
+    {
+      cerr << "Warning, data byte is NULL!" << endl;
+    }
+    else if (data[j] & 0x80)
+    {
+      switch (data[j])
+      {
+        case 0xe5:    // Select font
+          j+=2;
+          break;
+
+        case 0xe6:    // Font size
+          j+=2;
+          break;
+
+        case 0xe7:    // Color
+          j+=2;
+          break;
+
+        case 0xe8:    // Font weight
+          j+=2;
+          break;
+
+        case 0xe9:    // Italic
+          j++;
+          break;
+
+        case 0xea:    // Underline
+          j++;
+          break;
+
+        case 0xeb:    // Strikethrough
+          j++;
+          break;
+
+        case 0xec:    // Highlight
+          j++;
+          break;
+
+        case 0xef:    // XXX unknown
+          j+=3;
+          break;
+
+        case 0xc1:    // Character code
+          j++;
+          cout << (char)data[j];
+          break;
+
+        case 0xc2:    // Unknown
+          j+=2;
+          break;
+
+        case 0xc4:    // Control code
+          j++;
+          switch (data[j])
+          {
+            case 0x00:   // End of paragraph
+              break;
+
+            case 0x04:   // Tab character
+              cout << '\t';
+              break;
+
+            case 0x19:  // XXX unknown
+              j+=2;
+              break;
+
+            case 0x1b:  // XXX unknown
+              j+=6;
+              break;
+
+            default:
+              cerr << "Unknown control char: 0x" << hex << (unsigned)data[j] <<  
+                " at offset 0x" << paragraph_data_offset + j << endl;
+              break;
+          }
+          break;
+
+        case 0xc5:  // Reference to embedded data?
+          j+=2;
+          break;
+
+        case 0xf1:
+          j+=7;
+          break;
+
+        default:
+          cerr << "Unknown code: 0x" << hex << (unsigned)data[j] << 
+            " at offset 0x" << paragraph_data_offset + j << endl;
+          abort();
+      }
+    }
+    else
+      cout << (char)data[j];
+  }
+  cout << endl;
+
+  delete[] data;
+}/*}}}*/
+
+void drawing(istream& input)
+{
+  // Skip unknown
+  input.seekg(0xc, ios_base::cur);
+
+  unsigned drawing_size = read16(input);
+
+  cerr << "Drawing size: 0x" << hex << drawing_size << endl;
+  
+  // Skip drawing
+  input.seekg(drawing_size, ios_base::cur);
+}
+
+void align(istream& input, streampos start_offset)
+{
+  //cerr << "Start offset: 0x" << hex << start_offset << endl;
+  //cerr << "Current offset: 0x" << hex << input.tellg() << endl;
+  unsigned align = 4 - ((input.tellg() - start_offset) & 3);
+  //cerr << "Align: " << dec << align << endl;
+  input.seekg(align, ios_base::cur);
+}
+
 int main(int argc, char**argv)
 {
   ifstream input(argv[1], ofstream::binary);
@@ -96,12 +239,14 @@ int main(int argc, char**argv)
   input.seekg(0x6, ios_base::cur);
   /*unsigned paragraph_count_3 =*/ read16(input);
 
+#if VERBOSE
   cerr << "Paragraph count: " << dec << 
     paragraph_count /*<< " = " << 
                       paragraph_count_2 << " = " << 
                       paragraph_count_3*/ << endl;
+#endif
 
-    input.seekg(0x6, ios_base::cur);
+  input.seekg(0x6, ios_base::cur);
 
   //cerr << "Paragraph index starts at offset 0x" << hex << input.tellg() << endl;
 
@@ -132,142 +277,56 @@ int main(int argc, char**argv)
 
   for (unsigned i = 0; i < paragraph_count; i++)
   {
-    //cerr << "Paragraph " << dec << i << " starts at offset 0x" << hex << input.tellg() << endl;
+#if VERBOSE
+    cerr << "Paragraph " << dec << i << " starts at offset 0x" << hex << input.tellg() << endl;
+#endif
 
     unsigned code = read16(input);
-    if (0x41 != code)
+
+    streampos start_offset = input.tellg();
+
+    switch (code)
     {
-      cerr << "Not 0x41 but 0x" << hex << code << " - maybe a drawing?" << endl;
-    }
-
-    input.seekg(0x4, ios_base::cur);
-
-    /*unsigned plain_text_size      =*/ read16(input);
-    unsigned total_size           = read16(input);
-
-    //cerr << "Total size: 0x" << hex << total_size << endl;
-
-    input.seekg(0x14, ios_base::cur);
-
-    uint8_t* data = new uint8_t[total_size];
-
-    unsigned paragraph_data_offset = input.tellg();
-    input.read((char*)data, total_size);
-
-    unsigned align = 4 - (input.tellg() & 3);
-
-    input.seekg(align, ios_base::cur);
-
-    if (0x42 != read16(input))
-    {
-      cerr << "Not 0x42!" << endl;
-      return 1;
-    }
-
-    for (unsigned j = 0; j < total_size; j++)
-    {
-      if (data[j] == 0x00)
-      {
-        cerr << "Warning, data byte is NULL!" << endl;
-      }
-      else if (data[j] & 0x80)
-      {
-        switch (data[j])
+      case 0x41:
+        text(input);
+        
+        align(input, start_offset);
+        
+        code = read16(input);
+        if (code != 0x42)
         {
-          case 0xe5:    // Select font
-            j+=2;
-            break;
-
-          case 0xe6:    // Font size
-            j+=2;
-            break;
-
-          case 0xe7:    // Color
-            j+=2;
-            break;
-
-          case 0xe8:    // Font weight
-            j+=2;
-            break;
-
-          case 0xe9:    // Italic
-            j++;
-            break;
-
-          case 0xea:    // Underline
-            j++;
-            break;
-
-          case 0xeb:    // Strikethrough
-            j++;
-            break;
-
-          case 0xec:    // Highlight
-            j++;
-            break;
-
-          case 0xef:    // XXX unknown
-            j+=3;
-            break;
-
-          case 0xc1:    // Character code
-            j++;
-            cout << (char)data[j];
-            break;
-
-          case 0xc4:    // Control code
-            j++;
-            switch (data[j])
-            {
-              case 0x00:   // End of paragraph
-                break;
-
-              case 0x04:   // Tab character
-                cout << '\t';
-                break;
-
-              case 0x19:  // XXX unknown
-                j+=2;
-                break;
-
-              case 0x1b:  // XXX unknown
-                j+=6;
-                break;
-
-              default:
-                cerr << "Unknown control char: 0x" << hex << (unsigned)data[j] <<  
-                  " at offset 0x" << paragraph_data_offset + j << endl;
-                break;
-            }
-            break;
-
-          case 0xc5:  // Reference to embedded data?
-            j+=2;
-            break;
-
-          case 0xf1:
-            j+=7;
-            break;
-
-          default:
-            cerr << "Unknown code: 0x" << hex << (unsigned)data[j] << 
-              " at offset 0x" << paragraph_data_offset + j << endl;
-            abort();
+          cerr << "Not 0x42 but 0x" << hex << code << " at offset 0x" 
+            << hex << ((unsigned)input.tellg() - 2) << endl;
+          abort();
         }
-      }
-      else
-        cout << (char)data[j];
+        
+        start_offset = input.tellg();
+        input.seekg(0xe, ios_base::cur);
+        break;
+
+      case 0x43:
+        drawing(input);
+        break;
+
+      default:
+        cerr << "Unknown code 0x" << hex << code << " at 0x" <<
+          (input.tellg() - (streampos)2) << endl;
+        abort();
     }
-    cout << endl;
 
-    delete[] data;
+    input.seekg(4 - ((input.tellg() - start_offset) & 3), ios_base::cur);
 
-    input.seekg(0x10, ios_base::cur);
-
-    //cerr << "Paragraph " << dec << i << " ends at offset 0x" << hex << input.tellg() << endl;
+#if VERBOSE
+    cerr << "Paragraph " << dec << i << " ends at offset 0x" << hex << input.tellg() << endl;
+#endif
   }
 
+  if (0x82 != read16(input))
+    cerr << "Not 0x82!" << endl;
+
+#if VERBOSE
   cerr << "Decoding ends at offset 0x" << hex << input.tellg() << endl;
+#endif
 
   return 0;
 }
