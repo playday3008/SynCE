ba8c01c9d87bb9f5b46020fa7bed24e85b58a86b
Markus Biermaier <mbier@office-m.at>
2004-06-16 15:00:20 +0000

    Case with <> 1 distribution handled.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1725 599ab11a-0111-0410-8a50-d7a5b0953570

--- installer/PreInstall-Check.sh
+++ installer/PreInstall-Check.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #	Created:	Mit Jun 16 11:07:10 CEST 2004	by M. Biermaier	on linuxorange
-#	Version:	Mit Jun 16 13:54:02 CEST 2004	on linuxorange
+#	Version:	Wed Jun 16 16:30:42 CEST 2004	on macosbw
 #	$Id$
- 
+
 USAGE="usage: $0 [-r]\n
 -r ... Produce output in a shell-readable form"
 
@@ -34,13 +34,50 @@ fi
 # 2. Determine Distri
 #--------------------------------------------------
 #ls /etc/*-release
-DISTRI=`basename /etc/*-release -release`
-if [ $SHELL_READABLE ]
-then
-	echo "DISTRI=$DISTRI"
-else
-	echo "Your Distribution is:     $DISTRI"
-fi
+NUM_DISTRIS=`ls /etc/*-release | wc -l` 2>/dev/null
+# Omit leading spaces
+NUM_DISTRIS=$(($NUM_DISTRIS))
+
+case $NUM_DISTRIS in
+	0)
+		if [ $SHELL_READABLE ]
+		then
+			echo "NUM_DISTRIS=$NUM_DISTRIS"
+		else
+			echo "No Distribution-Info found!"
+		fi
+		;;
+
+	1)
+		DISTRI=`basename /etc/*-release -release`
+		if [ $SHELL_READABLE ]
+		then
+			echo "DISTRI=$DISTRI"
+		else
+			echo "Your distribution is:     $DISTRI"
+		fi
+		;;
+	*)
+		if [ $SHELL_READABLE ]
+		then
+			echo "NUM_DISTRIS=$NUM_DISTRIS"
+		else
+			echo "#--------------------------------------------------"
+			echo "ATTENTION: $NUM_DISTRIS distributions found!"
+			echo "#--------------------------------------------------"
+		fi
+		for RELEASE in /etc/*-release
+		do
+			DISTRI=`basename $RELEASE -release`
+			if [ $SHELL_READABLE ]
+			then
+				echo "DISTRI=$DISTRI"
+			else
+				echo " a distribution is:       $DISTRI"
+			fi
+		done
+		;;
+esac
 
 
 # 3. Check if PyQt is installed.
