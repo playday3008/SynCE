40a07282775cb4490f2aa2ec80508b54cb2aef2d
David Eriksson <david@activout.se>
2004-07-10 14:10:54 +0000

    Added E_OUTOFMEMORY
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1755 599ab11a-0111-0410-8a50-d7a5b0953570

--- libsynce/lib/synce_types.h
+++ libsynce/lib/synce_types.h
@@ -81,6 +81,7 @@ typedef int32_t  HRESULT;
 #define E_ACCESSDENIED  0x80070005
 #define E_FAIL          0x80004005
 #define E_HANDLE        0x80070006
+#define E_OUTOFMEMORY   0x8007000E
 #define E_INVALIDARG    0x80070057
 #define E_NOINTERFACE   0x80004002
 #define E_NOTIMPL       0x80004001
