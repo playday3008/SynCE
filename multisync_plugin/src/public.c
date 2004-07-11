bdd94bc56ef4412cb1d3905345f5a9fbda7e222b
David Eriksson <david@activout.se>
2004-07-11 15:06:53 +0000

    Use syncobj_modify_list
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1766 599ab11a-0111-0410-8a50-d7a5b0953570

--- multisync_plugin/src/public.c
+++ multisync_plugin/src/public.c
@@ -12,6 +12,8 @@
 #include <string.h>
 #include <gnome.h>
 
+#define USE_SYNCOBJ_MODIFY_LIST 1
+
 /*
    This file contains the public functions used by MultiSync
  */
@@ -141,6 +143,386 @@ exit:
 	synce_trace("<-----");
 }
 
+#if USE_SYNCOBJ_MODIFY_LIST
+
+typedef struct
+{
+  changed_object* object;
+  syncobj_modify_result* result;
+} ObjectAndResult;
+
+typedef struct
+{
+  /* Global data */
+  SynceConnection* connection;
+  int type_index;
+  GList* objects;
+  /* Data for the current object */
+  unsigned object_index;
+  ObjectAndResult* oar;
+  uint8_t* data;
+  uint8_t* current;
+  size_t data_left;
+} ObjectReaderParameters;
+
+static ssize_t
+object_reader(/*{{{*/
+    uint32_t type_id, unsigned object_index, uint8_t* data, size_t data_size, void* cookie)
+{
+  ssize_t result = -1;
+  ObjectReaderParameters* parameters = (ObjectReaderParameters*)cookie;
+  if (!parameters)
+    goto exit;
+  
+  if (parameters->object_index != object_index)
+  {
+    bool success = false;
+    uint32_t dummy_id;
+
+    parameters->object_index = object_index;
+    parameters->oar = (ObjectAndResult*)g_list_nth_data(
+        parameters->objects,
+        object_index);
+
+    if (!parameters->oar)
+    {
+      synce_error("Failed to get object %i from list", object_index);
+      goto exit;
+    }
+
+    /* Convert to native data */
+    switch (parameters->type_index)/*{{{*/
+    {
+      case INDEX_APPOINTMENT:
+        success = rra_appointment_from_vevent(
+            parameters->oar->object->comp,
+            &dummy_id,
+            &parameters->data,
+            &parameters->data_left,
+            RRA_APPOINTMENT_UTF8,
+            &parameters->connection->timezone);
+        break;
+
+      case INDEX_CONTACT:
+        success = rra_contact_from_vcard(
+            parameters->oar->object->comp,
+            &dummy_id,
+            &parameters->data,
+            &parameters->data_left,
+            RRA_CONTACT_UTF8 | RRA_CONTACT_VERSION_2_1);
+        break;
+
+      case INDEX_TASK:
+        success = rra_task_from_vtodo(
+            parameters->oar->object->comp,
+            &dummy_id,
+            &parameters->data,
+            &parameters->data_left,
+            RRA_TASK_UTF8,
+            &parameters->connection->timezone);
+        break;
+
+      default:
+        synce_error("Unexpected index: %i", index);
+        sync_set_requestfailederror(
+            _("Unexpected object type"),
+            parameters->connection->handle);
+        goto exit;
+    }/*}}}*/
+
+    if (!success)
+    {
+      synce_error("Data conversion failed for type %08x and object %d",
+          parameters->connection->type_ids[parameters->type_index], object_index);
+      sync_set_requestfailederror(
+          _("Failed to convert object"),
+          parameters->connection->handle);
+      goto exit;
+    }
+
+    parameters->current = parameters->data;
+  }
+
+  result = MIN(parameters->data_left, data_size);
+
+  if (result)
+  {
+    memcpy(data, parameters->current, result);
+
+    parameters->current   += result;
+    parameters->data_left -= result;
+
+    if (parameters->data_left == 0)
+    {
+      /* Clean up! */
+      switch (parameters->type_index)/*{{{*/
+      {
+        case INDEX_APPOINTMENT:
+          rra_appointment_free_data(parameters->data);
+          break;
+
+        case INDEX_CONTACT:
+          rra_contact_free_data(parameters->data);
+          break;
+
+        case INDEX_TASK:
+          rra_task_free_data(parameters->data);
+          break;
+      }/*}}}*/
+
+      parameters->data    = NULL;
+      parameters->current = NULL;
+    }
+  }
+
+exit:
+  return result;
+}/*}}}*/
+
+/*
+   The objects list contains points to ObjectAndResult structs
+   */
+static bool
+put_objects(SynceConnection* connection, int index, GList* objects, uint32_t flags)/*{{{*/
+{
+  bool success = false;
+  ObjectReaderParameters parameters;
+  int count = g_list_length(objects);
+  uint32_t* ids = g_new0(uint32_t, count);
+  uint32_t* received_ids = g_new0(uint32_t, count);
+  GList* item;
+  int i;
+
+  /* Provide IDs for existing objects */
+  if (flags == RRA_SYNCMGR_UPDATE_OBJECT)
+  {
+    for (item = objects, i = 0; item != NULL; item = g_list_next(item), i++)
+    {
+      ObjectAndResult* oar = (ObjectAndResult*)item->data;
+
+      if (oar->object->uid)
+        ids[i] = strtol(oar->object->uid, NULL, 16);
+
+      if (!ids[i])
+      {
+        synce_warning("Unexpected uid '%s', will become a new object!", oar->object->uid);
+      }
+    }
+  }
+
+  memset(&parameters, 0, sizeof(parameters));
+  parameters.connection = connection;
+  parameters.type_index = index;
+  parameters.objects    = objects;
+  parameters.object_index = -1;
+
+  /* Put objects */
+  if (!rra_syncmgr_put_multiple_objects(
+        connection->syncmgr,
+        connection->type_ids[index],
+        count,
+        ids,
+        received_ids,
+        flags,
+        object_reader,
+        &parameters))
+  {
+    synce_error("Failed to put %i objects of type %08x with flags %08x", 
+        count, connection->type_ids[index], flags);
+    goto exit;
+  }
+
+  /* Handle IDs for new objects */
+  /*if (flags == RRA_SYNCMGR_NEW_OBJECT)*/
+  {
+    for (item = objects, i = 0; item != NULL; item = g_list_next(item), i++)
+    {
+      ObjectAndResult* oar = (ObjectAndResult*)item->data;
+
+      if (received_ids[i] == 0xffffffff)
+      {
+        oar->result->result = SYNC_MSG_MODIFYERROR;
+      }
+      else
+      {
+        oar->result->result = SYNC_MSG_NOMSG;
+        oar->result->returnuid = g_strdup_printf("%08x",
+            received_ids[i]);
+      }
+    }
+  }
+ 
+  success = true;
+
+exit:
+  g_free(ids);
+  g_free(received_ids);
+  return success;
+}/*}}}*/
+
+static void
+append_object_and_result(GList** oar_list, changed_object* object, GList** results)/*{{{*/
+{
+  ObjectAndResult* oar = g_new0(ObjectAndResult, 1);
+  
+  oar->object = object;
+  oar->result = g_new0(syncobj_modify_result, 1);
+  
+  *oar_list = g_list_append(*oar_list, oar);
+  *results  = g_list_append(*results, oar->result);
+}/*}}}*/
+
+static void g_free_GFunc(void* data, void* cookie)
+{
+  g_free(data);
+}
+
+/* syncobj_modify_list()
+
+   Modify many objects in the database.
+
+   The changes parameter is a list of changed_object structs.
+*/
+void 
+syncobj_modify_list(SynceConnection* connection, GList* all_changes) 
+{
+  GList* item = NULL;
+  GList* deleted = NULL;
+  GList* added[INDEX_MAX];
+  GList* modified[INDEX_MAX];
+  GList *results = NULL;
+  int i;
+
+  memset(added,     0, sizeof(added));
+  memset(modified,  0, sizeof(modified));
+
+  synce_trace("----->");
+
+  if (!synce_join_thread(connection))
+  {
+    sync_set_requestfailederror(
+        _("Failed to wait for thread termination"),
+        connection->handle);
+    goto exit;
+  }
+
+  if (!connection->syncmgr || !rra_syncmgr_is_connected(connection->syncmgr))
+  {
+    sync_set_requestfailederror(
+        _("The SynCE synchronization manager is not connected. Please restart MultiSync."),
+        connection->handle);
+    goto exit;
+  }
+
+  /* Separare changes into deleted, added and modified */
+  synce_trace("%i changes total", g_list_length(all_changes));
+  for (item = all_changes; item != NULL; item = g_list_next(item))/*{{{*/
+  {
+    changed_object* object = (changed_object*)item->data;
+    int index = synce_index_from_sync_object_type(object->object_type);
+    
+    if (index == INDEX_MAX)
+    {
+      synce_error("Unexpected type: %i", object->object_type);
+      continue;
+    }
+
+    switch (object->change_type)
+    {
+      case SYNC_OBJ_HARDDELETED:
+        append_object_and_result(&deleted, object, &results);
+        break;
+        
+      case SYNC_OBJ_SOFTDELETED:
+        /* Ignore these */
+        break;
+
+      case SYNC_OBJ_ADDED:
+        append_object_and_result(&added[index], object, &results);
+        break;
+
+      case SYNC_OBJ_MODIFIED:
+        append_object_and_result(&modified[index], object, &results);
+        break;
+
+      default:
+        synce_warning("Unknown change type: %i", object->change_type);
+        break;
+    }
+  }/*}}}*/
+
+  /* Can currently only delete one item at a time, so we do that... */
+  synce_trace("%i items deleted", g_list_length(deleted));
+  for (item = deleted; item != NULL; item = g_list_next(item))/*{{{*/
+  {
+    ObjectAndResult* oar = (ObjectAndResult*)item->data;
+    changed_object* object = oar->object;
+    int index;
+    uint32_t object_id;
+    index = synce_index_from_sync_object_type(object->object_type);
+    
+    if (INDEX_MAX == index)
+    {
+      synce_error("Unexpected type: %i", object->object_type);
+      sync_set_requestfailederror(
+          _("Unexpected object type"),
+          connection->handle);
+      goto exit;
+    }
+
+    object_id = strtol(object->uid, NULL, 16);
+
+    if (!object_id)
+    {
+      synce_error("Unexpected uid: '%s'", object->uid);
+      sync_set_requestfailederror(
+          _("Unexpected uid"),
+          connection->handle);
+      goto exit;
+    }
+
+    if (!rra_syncmgr_delete_object(connection->syncmgr,
+          connection->type_ids[index],
+          object_id))
+    {
+      synce_error("Failed to delete object with type %08x and ID %08x", 
+          connection->type_ids[index],
+          object_id);
+      oar->result->result = SYNC_MSG_REQFAILED;
+    }
+  }/*}}}*/
+  
+  for (i = 0; i < INDEX_MAX; i++)
+  {
+    synce_trace("%i items added of type index %i", g_list_length(added[i]), i);
+    synce_trace("%i items modified of type index %i", g_list_length(modified[i]), i);
+
+    if (!put_objects(connection, i, added[i], RRA_SYNCMGR_NEW_OBJECT))
+      break;
+
+    if (!put_objects(connection, i, modified[i], RRA_SYNCMGR_UPDATE_OBJECT))
+      break;
+  }
+
+  sync_set_requestdata(results, connection->handle);
+  /* implicited by the above? sync_set_requestdone(connection->handle); */
+
+exit:
+  g_list_foreach(deleted, g_free_GFunc, NULL);
+  g_list_free(deleted);
+  for (i = 0; i < INDEX_MAX; i++)
+  {
+    g_list_foreach(added[i], g_free_GFunc, NULL);
+    g_list_free   (added[i]);
+    g_list_foreach(modified[i], g_free_GFunc, NULL);
+    g_list_free   (modified[i]);
+  }
+  synce_create_thread(connection);
+  synce_trace("<-----");
+}
+
+#else /* USE_SYNCOBJ_MODIFY_LIST */
+
 /* syncobj_modify() 
 
    Modify or add an object in the database. This is called by the sync
@@ -167,7 +549,7 @@ exit:
    returnuidlen.
  */
 
-void syncobj_modify(
+void syncobj_modify(/*{{{*/
     SynceConnection* connection,
     char* object, 
     char* uid,
@@ -371,15 +753,14 @@ exit:
   synce_create_thread(connection);
 	synce_trace("<-----");
   return;
-}
-
+}/*}}}*/
 
 /* syncobj_delete() 
 
    Delete an object from the database. If the argument softdelete is 
    true, then this object is deleted by the sync engine for storage reasons.
 */
-void syncobj_delete(
+void syncobj_delete(/*{{{*/
     SynceConnection* connection, 
     char* uid,
     sync_object_type objtype, 
@@ -444,7 +825,9 @@ void syncobj_delete(
 
 exit:
 	synce_trace("<-----");
-}
+}/*}}}*/
+
+#endif /* USE_SYNCOBJ_MODIFY_LIST */
 
 /* syncobj_get_recurring()
 
