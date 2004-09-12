022321c23b02e404e323a6369b7d7ae70671c43d
David Eriksson <david@activout.se>
2004-09-12 18:06:38 +0000

    Improve header script
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1911 599ab11a-0111-0410-8a50-d7a5b0953570

--- htdocs/header.php
+++ htdocs/header.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<title>SynCE<?php if ($title) echo ":: $title"; ?></title>
+	<title>SynCE<?php if ($title) echo " :: $title"; ?></title>
 	<link href="synce.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
