12d60be4c31827c262037ee2f89f40dd246bfec8
David Eriksson <david@activout.se>
2004-09-14 11:10:00 +0000

    Make it possible to view the built-in help even if not connected.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1913 599ab11a-0111-0410-8a50-d7a5b0953570

--- rra/src/rra-partners.c
+++ rra/src/rra-partners.c
@@ -8,98 +8,37 @@
 #include <strings.h>
 #include <stdlib.h>
 
+typedef enum
+{
+  COMMAND_HELP,
+  COMMAND_STATUS,
+  COMMAND_CREATE,
+  COMMAND_REPLACE,
+} COMMAND;
+
+
 int main(int argc, char** argv)
 {
-	int result = 1;
+  int result = 1;
   RRA_Matchmaker* matchmaker = NULL;
-	DWORD current_partner;
-	HRESULT hr;
-	int i;
-  const char* command = "";
-
-	hr = CeRapiInit();
-	if (FAILED(hr))
-	{
-		synce_error("Failed to initialize RAPI");
-		goto exit;
-	}
-
-  matchmaker = rra_matchmaker_new();
-  if (!matchmaker)
-  {
- 		synce_error("Failed to create match-maker");
-		goto exit;
- }
+  DWORD current_partner;
+  uint32_t index = 0;
+  HRESULT hr;
+  int i;
+  COMMAND command = COMMAND_HELP; 
+  const char* command_string = "";
 
   if (argc >= 2)
-    command = argv[1];
+    command_string = argv[1];
 
-  if (0 == strcasecmp(command, "status"))/*{{{*/
-  {
-    if (!rra_matchmaker_get_current_partner(matchmaker, &current_partner))
-    {
-      fprintf(stderr, "Failed to get current partner index");
-      goto exit;
-    }
+  if (0 == strcasecmp(command_string, "status"))
+    command = COMMAND_STATUS;
+  else if (0 == strcasecmp(command_string, "create"))
+    command = COMMAND_CREATE;
+  else if (0 == strcasecmp(command_string, "replace"))
+    command = COMMAND_REPLACE;
 
-    printf("Current partner index: %i\n", current_partner);
-
-    for (i = 1; i <= 2; i++)
-    {
-      uint32_t id = 0;
-      char* name = NULL;
-
-      if (rra_matchmaker_get_partner_id(matchmaker, i, &id))
-      {
-        printf("Partner %i id:    0x%08x\n", i, id);
-      }
-
-      if (rra_matchmaker_get_partner_name(matchmaker, i, &name))
-      {
-        printf("Partner %i name:  \"%s\"\n", i, name);
-        rra_matchmaker_free_partner_name(name);
-      }
-    }
-  }/*}}}*/
-  else if (0 == strcasecmp(command, "create"))/*{{{*/
-  {
-    uint32_t index = 0;
-    if (rra_matchmaker_create_partnership(matchmaker, &index))
-    {
-      printf("Partnership creation succeeded. Using partnership index %i.\n", index);
-    }
-    else
-    {
-      fprintf(stderr, "Partnership creation failed. Maybe there is no empty partnership slot?\n");
-      goto exit;
-    }
-  }/*}}}*/
-  else if (0 == strcasecmp(command, "replace"))/*{{{*/
-  {
-    uint32_t index = 0;
-    
-    if (argc >= 3)
-      index = atol(argv[2]);
-
-    if (index == 1 || index == 2)
-    {
-      if (rra_matchmaker_replace_partnership(matchmaker, index))
-      {
-        printf("Partnership replacement succeeded.\n");
-      }
-      else
-      {
-        fprintf(stderr, "Partnership replacement failed.\n");
-      }
-    }
-    else
-    {
-      fprintf(stderr, "Invalid or missing index of partnership to replace.\n");
-      goto exit;
-    }
-    
-  }/*}}}*/
-  else
+  if (command == COMMAND_HELP)
   {
     printf(
         "The purpose of this program is to manage partnerships with the currently\n"
@@ -114,13 +53,94 @@ int main(int argc, char** argv)
         "\tINDEX    The partnership index (1 or 2)\n",
         argv[0]
         );
+    goto exit;
   }
 
-	result = 0;
-	
+  hr = CeRapiInit();
+  if (FAILED(hr))
+  {
+    synce_error("Failed to initialize RAPI");
+    goto exit;
+  }
+
+  matchmaker = rra_matchmaker_new();
+  if (!matchmaker)
+  {
+    synce_error("Failed to create match-maker");
+    goto exit;
+  }
+
+  switch (command)
+  {
+    case COMMAND_STATUS:
+      if (!rra_matchmaker_get_current_partner(matchmaker, &current_partner))
+      {
+        fprintf(stderr, "Failed to get current partner index");
+        goto exit;
+      }
+
+      printf("Current partner index: %i\n", current_partner);
+
+      for (i = 1; i <= 2; i++)
+      {
+        uint32_t id = 0;
+        char* name = NULL;
+
+        if (rra_matchmaker_get_partner_id(matchmaker, i, &id))
+        {
+          printf("Partner %i id:    0x%08x\n", i, id);
+        }
+
+        if (rra_matchmaker_get_partner_name(matchmaker, i, &name))
+        {
+          printf("Partner %i name:  \"%s\"\n", i, name);
+          rra_matchmaker_free_partner_name(name);
+        }
+      }
+
+    case COMMAND_CREATE:
+      if (rra_matchmaker_create_partnership(matchmaker, &index))
+      {
+        printf("Partnership creation succeeded. Using partnership index %i.\n", index);
+      }
+      else
+      {
+        fprintf(stderr, "Partnership creation failed. Maybe there is no empty partnership slot?\n");
+        goto exit;
+      }
+      break;
+
+    case COMMAND_REPLACE:
+      if (argc >= 3)
+        index = atol(argv[2]);
+
+      if (index == 1 || index == 2)
+      {
+        if (rra_matchmaker_replace_partnership(matchmaker, index))
+        {
+          printf("Partnership replacement succeeded.\n");
+        }
+        else
+        {
+          fprintf(stderr, "Partnership replacement failed.\n");
+        }
+      }
+      else
+      {
+        fprintf(stderr, "Invalid or missing index of partnership to replace.\n");
+        goto exit;
+      }
+      break;
+
+    default:
+      goto exit;
+  }
+
+  result = 0;
+
 exit:
   rra_matchmaker_destroy(matchmaker);
   CeRapiUninit();
-	return result;
+  return result;
 }
 
