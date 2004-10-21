aeb7be08b6bcca70beb90eaca1102b4d47984a97
Christian Fremgen <Christian.Fremgen@ith-icoserve.com>
2004-10-21 16:05:33 +0000

    changes for release pocketpccommunication_0_1_1.tgz
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@1934 599ab11a-0111-0410-8a50-d7a5b0953570

--- htdocs/kde/konnector/news.php
+++ htdocs/kde/konnector/news.php
@@ -4,6 +4,16 @@
 <br>
 <h3>News</h3>
 <ul>
+    <li>Thu Oct 21 2004: released pocketpccommunication_0_1_1.tgz</li>
+   Changes: 
+    <ul>
+        <li>it is now just one package and not two as in the first release</li>
+        it is called pocketpccommunication_0_1_1.tgz
+        <li>as some had problems with missing kabcresourcenull. h I added this to the package</li>
+        <li>the code is now available via cvs on sourceforge</li>
+        </li>there were some bugs concerning the synchronization of events and todos which
+        should be squished :)</li>
+    </ul> 
     <li>Thu Oct  7 2004: put the documentation and archives online</li>
 </ul>
 
