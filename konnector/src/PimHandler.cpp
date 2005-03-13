fca3ec22103737a90712bd9a2f718e780eb0846b
Volker Christian <me@vchrist.at>
2005-03-13 15:27:09 +0000

    Removed useless files
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2002 599ab11a-0111-0410-8a50-d7a5b0953570

--- konnector/src/PimHandler.cpp
+++ konnector/src/PimHandler.cpp
@@ -22,16 +22,8 @@
 
 namespace pocketPCCommunication {
 
-
-PimHandler::PimHandler(const QString& p_pdaName)
-    : m_pdaName(p_pdaName), m_appName("POCKETPCCOMM"), m_keyName("REMOTE-ID"), m_partnerId("")
-{
-    m_rra = new Rra(p_pdaName);
-}
-
-
 PimHandler::PimHandler (KSharedPtr<Rra> p_rra)
-    : m_pdaName(""), m_appName("POCKETPCCOMM"), m_keyName("REMOTE-ID"), m_partnerId("")
+    : m_pdaName("")
 {
     m_pdaName = p_rra->getPdaName();
     m_rra = p_rra;
@@ -41,145 +33,10 @@ PimHandler::~PimHandler()
 {
 }
 
-
-bool PimHandler::getIdStatusPro (QMap<QString,RecordType>& p_statusMap, const uint32_t& p_typeId)
-{
-    //m_rra->connect();
-
-    struct Rra::ids ids;
-    /*
-    if (!m_rra->getIds (p_typeId, &ids))
-    {
-        m_rra->disconnect();
-        return false;
-    }
-    */
-    if (!m_rra->getIds (p_typeId, &ids)) {
-        kdDebug(2120) << "------------------------pppppppppppppppp" << endl;
-        return false;
-    }
-
-    // now we have the necessary data...
-    // iterate over the complete data structure and fill the map
-
-    fillStatusMap (p_statusMap, ids, CHANGED);
-    fillStatusMap (p_statusMap, ids, DELETED);
-    fillStatusMap (p_statusMap, ids, UNCHANGED);
-
-    //m_rra->disconnect();
-    return true;
-}
-
-
-void PimHandler::fillStatusMap(QMap<QString,RecordType>& p_statusMap, const struct Rra::ids& p_ids, RecordType p_recType)
-{
-    //kdDebug() << "PimHandler::fillStatusMap" << endl;
-
-    QValueList<uint32_t>::const_iterator it;
-    QValueList<uint32_t>::const_iterator end;
-
-    switch (p_recType)
-    {
-        case CHANGED:
-            it = p_ids.changedIds.begin();
-            end = p_ids.changedIds.end();
-            //size = p_ids.changedIds.size();
-            break;
-        case DELETED:
-            it = p_ids.deletedIds.begin();
-            end = p_ids.deletedIds.end();
-            //size = p_ids.deletedIds.size();
-            break;
-        case UNCHANGED:
-            it = p_ids.unchangedIds.begin();
-            end = p_ids.unchangedIds.end();
-            //size = p_ids.unchangedIds.size();
-            break;
-        case ALL: // not reasonable to have ALL here! added to avoid warning of gcc
-            break;
-    }
-
-    for (; it != end; ++it)
-    {
-        //kdDebug() << "     " << QString::number(*it,16).rightJustify(8, '0') << endl;
-        p_statusMap["RRA-ID-" + QString::number(*it,16).rightJustify(8, '0')] = p_recType; // number must be padded!
-    }
-}
-
-
-void PimHandler::deleteEntries (const struct Rra::ids& p_ids, const uint32_t& p_typeId, RecordType p_recType)
-{
-    kdDebug() << "PimHandler::deleteEntries" << endl;
-
-    m_rra->connect();
-    QValueList<uint32_t>::const_iterator it;
-    QValueList<uint32_t>::const_iterator end;
-
-    switch (p_recType)
-    {
-        case CHANGED:
-            it = p_ids.changedIds.begin();
-            end = p_ids.changedIds.end();
-            break;
-        case DELETED:
-            it = p_ids.deletedIds.begin();
-            end = p_ids.deletedIds.end();
-            break;
-        case UNCHANGED:
-            it = p_ids.unchangedIds.begin();
-            end = p_ids.unchangedIds.end();
-            break;
-        case ALL: // not reasonable to have ALL here! added to avoid warning of gcc
-            break;
-    }
-
-    for (; it != end; ++it)
-    {
-        kdDebug() << "deleting: " << p_typeId << " " << *it << endl;
-        m_rra->deleteObject (p_typeId, *it);
-    }
-
-    m_rra->disconnect();
-}
-
-
 void PimHandler::deleteSingleEntry (const uint32_t& p_typeId, const uint32_t& p_objectId)
 {
     m_rra->deleteObject (p_typeId, p_objectId);
 }
-
-
-QString PimHandler::getPartnerId()
-{
-    if (m_partnerId.isEmpty())
-    {
-        m_rra->connect();
-        uint32_t id;
-        m_rra->getMatchMaker()->get_partner_id(1, &id);
-        m_partnerId = QString::number (id, 16);
-        m_rra->disconnect();
-    }
-    return m_partnerId;
-}
-
-
-void PimHandler::clearIdPairs ()
-{
-    m_idPairs.clear();
-}
-
-
-void PimHandler::addIdPair (const QString p_device, const QString p_local)
-{
-    m_idPairs.push_back (qMakePair (p_device, p_local));
-}
-
-
-QValueList<QPair<QString, QString> > PimHandler::getIdPairs ()
-{
-    return m_idPairs;
-}
-
 
 uint32_t PimHandler::getOriginalId(const QString& p_id)
 {
@@ -188,11 +45,4 @@ uint32_t PimHandler::getOriginalId(const QString& p_id)
 
     return id.remove("RRA-ID-").toUInt(&ok, 16);
 }
-
-
-bool PimHandler::isARraId (const QString& p_id)
-{
-    return p_id.startsWith ("RRA-ID-");
-}
-
 };
--- konnector/src/PimHandler.h
+++ konnector/src/PimHandler.h
@@ -37,7 +37,7 @@ public:
     /** Just a simple constructor to initialize the members.
       * @param p_pdaName as the name says :)
       */
-    PimHandler(const QString& p_pdaName);
+//    PimHandler(const QString& p_pdaName);
 
     /** in case we already have a Rra-Connection we can use this constuctor
       */
@@ -49,20 +49,18 @@ public:
       * status: CHANGED, DELETED, UNCHANGED
       * @see getIdStatusPro for how it is done
       */
-//    virtual bool getIdStatus (QMap<QString, RecordType>& p_statusMap) = 0;
 
-    virtual QString getPartnerId ();
 
-    virtual QValueList<QPair<QString, QString> > getIdPairs ();
+//    virtual QValueList<QPair<QString, QString> > getIdPairs ();
 
 
 protected:
 
-    bool getIdStatusPro (QMap<QString, RecordType>& p_statusMap, const uint32_t& p_typeId);
+//    bool getIdStatusPro (QMap<QString, RecordType>& p_statusMap, const uint32_t& p_typeId);
 
     /** A method to permanently delete entries on the pda!
       */
-    virtual void deleteEntries (const struct Rra::ids& p_ids, const uint32_t& p_typeId, RecordType p_recType);
+//    virtual void deleteEntries (const struct Rra::ids& p_ids, const uint32_t& p_typeId, RecordType p_recType);
 
     /** This abstract method must be overwritten within each subclass to fill appropriate members/static members
       * with the type id.
@@ -72,25 +70,25 @@ protected:
     virtual void deleteSingleEntry (const uint32_t& p_typeId, const uint32_t& p_objectId);
 
     uint32_t getOriginalId (const QString& p_id);
-    bool isARraId (const QString& p_id);
+//    bool isARraId (const QString& p_id);
 
-    void clearIdPairs ();
-    void addIdPair (const QString p_device, const QString p_local);
+//    void clearIdPairs ();
+//    void addIdPair (const QString p_device, const QString p_local);
 
     QString         m_pdaName;   /**< The name of the device we want to use. */
     //Rra             m_rra;       /**< Connection handler. */
     KSharedPtr<Rra> m_rra;       /**< Connection handler. Shared to have one instance in many objects. */
     //Rra*            m_rra;
 
-    const QString    m_appName;
-    const QString    m_keyName;
+//    const QString    m_appName;
+//    const QString    m_keyName;
 
 
 private:
-    void fillStatusMap (QMap<QString, RecordType>& p_statusMap, const struct Rra::ids& p_ids, RecordType p_recType);
+//    void fillStatusMap (QMap<QString, RecordType>& p_statusMap, const struct Rra::ids& p_ids, RecordType p_recType);
 
-    QString m_partnerId;
-    QValueList<QPair<QString, QString> >    m_idPairs;
+//    QString m_partnerId;
+//    QValueList<QPair<QString, QString> >    m_idPairs;
 };
 
 };
