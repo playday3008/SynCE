1e07c0bae69e6f2b981dd5042d6a77a3ac0f1887
David Eriksson <david@activout.se>
2005-08-06 13:11:41 +0000

    Set CFLAGS in environment
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2142 599ab11a-0111-0410-8a50-d7a5b0953570

--- rra/rebuild.sh
+++ rra/rebuild.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-./cvsclean && ./bootstrap && ./configure --enable-recurrence --prefix=/var/tmp/synce --with-libsynce=/var/tmp/synce --with-librapi2=/var/tmp/synce && make
+set -e
+./cvsclean 
+./bootstrap 
+export CFLAGS="-Wall -Werror"
+./configure \
+  --enable-recurrence \
+  --prefix=/var/tmp/synce \
+  --with-libsynce=/var/tmp/synce \
+  --with-librapi2=/var/tmp/synce
+make
