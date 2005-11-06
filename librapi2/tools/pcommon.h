6181f2e6d7c0266f17f9f3ae66a0d190c7f69700
David Eriksson <david@activout.se>
2005-11-06 19:16:25 +0000

    Make it possible to use from C++
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2170 599ab11a-0111-0410-8a50-d7a5b0953570

--- librapi2/tools/pcommon.h
+++ librapi2/tools/pcommon.h
@@ -3,6 +3,11 @@
 
 #include <synce.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 void convert_to_backward_slashes(char* path);
 bool is_remote_file(const char* filename);
 WCHAR* adjust_remote_path(WCHAR* old_path, bool free_path);
@@ -20,6 +25,10 @@ AnyFile* anyfile_open(const char* filename, ANYFILE_ACCESS access);
 void anyfile_close(AnyFile* file);
 bool anyfile_read (AnyFile* file, unsigned char* buffer, size_t bytes, size_t* bytesAccessed);
 bool anyfile_write(AnyFile* file, unsigned char* buffer, size_t bytes, size_t* bytesAccessed);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
