273a84dabc217afd5e0a8aee1959eb5d6b50d280
Volker Christian <me@vchrist.at>
2006-02-06 22:36:15 +0000

    Had much problems with cvs (it was my fault)
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2214 599ab11a-0111-0410-8a50-d7a5b0953570

---
+++ konnector/syncedevicekonnector/todohandler.h
@@ -0,0 +1,61 @@
+//
+// C++ Interface: todohandler
+//
+// Description:
+//
+//
+// Author: Christian Fremgen; Volker Christian <cfremgen@users.sourceforge.net>, (C) 2005
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef POCKETPCCOMMUNICATIONTODOHANDLER_H
+#define POCKETPCCOMMUNICATIONTODOHANDLER_H
+
+
+#include "pimhandler.h"
+#include "recordtype.h"
+#include "todosyncee.h"
+#include <kitchensync/idhelper.h>
+#include <libkdepim/progressmanager.h>
+
+
+namespace KPIM {
+    class ProgressItem;
+}
+
+namespace PocketPCCommunication {
+
+/**
+@author Christian Fremgen cfremgen@users.sourceforge.net, Volker Christian voc@users.sourceforge.net
+*/
+class TodoHandler : public PimHandler
+{
+public:
+    TodoHandler (Rra *p_rra, KSync::KonnectorUIDHelper *mUidHelper );
+
+    bool init();
+
+    virtual ~TodoHandler();
+
+    int retrieveTodoListFromDevice(KCal::Todo::List &mTodoList, QValueList<uint32_t> &idList);
+    int fakeTodoListFromDevice(KCal::Todo::List &mTodoList, QValueList<uint32_t> &idList);
+    bool getIds();
+    int getTodoListFromDevice(KCal::Todo::List &mTodoList, int mRecType);
+    bool readSyncee(KSync::TodoSyncee *mCalendarSyncee, bool firstSync);
+    void getTodos (KCal::Todo::List& p_addressees, KSync::SyncEntry::PtrList p_ptrList );
+    void getTodosAsFakedTodos(KCal::Todo::List& p_todos, KSync::SyncEntry::PtrList p_ptrList );
+    bool writeSyncee(KSync::TodoSyncee *mCalendarSyncee);
+    void insertIntoCalendarSyncee(KSync::TodoSyncee *mCalendarSyncee, KCal::Todo::List &list, int state);
+
+    void addTodos    (KCal::Todo::List& p_todoList);
+    void updateTodos (KCal::Todo::List& p_todoList);
+    void removeTodos (KCal::Todo::List& p_todoList);
+
+private:
+    KSync::KonnectorUIDHelper *mUidHelper;
+};
+
+}
+
+#endif
