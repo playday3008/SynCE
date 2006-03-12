a1a160a777609c3da47b12f92a5e761c658e7982
Nabil Sayegh <postgresql@e-trolley.de>
2006-03-12 17:11:47 +0000

    Support for non-standard fields
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2300 599ab11a-0111-0410-8a50-d7a5b0953570

---
+++ rra/lib/synce_ids.h
@@ -0,0 +1,12 @@
+#ifndef __synce_ids_h__
+#define __synce_ids_h__
+
+#define SYNCE_ID_BASE 0xd000
+#define ID_IMADDRESS  (SYNCE_ID_BASE+0)
+#define ID_NICKNAME  (SYNCE_ID_BASE+1)
+#define ID_MESSAGING_ICQ (SYNCE_ID_BASE+2)
+#define ID_MESSAGING_XMPP (SYNCE_ID_BASE+3)
+#define ID_MESSAGING_MSN (SYNCE_ID_BASE+4)
+#define ID_MESSAGING_GADU (SYNCE_ID_BASE+5)
+
+#endif
