e376a0a5073714e52b6abcd3400435ec615e4975
David Eriksson <david@activout.se>
2006-09-14 13:27:17 +0000

    Configuration management fixes
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2573 599ab11a-0111-0410-8a50-d7a5b0953570

--- rra/rebuild.sh
+++ rra/rebuild.sh
@@ -3,9 +3,8 @@ set -e
 ./cvsclean 
 ./bootstrap 
 export CFLAGS="-Wall -Werror"
+export PKG_CONFIG_PATH="/var/tmp/synce/lib/pkgconfig:$PKG_CONFIG_PATH"
 ./configure \
   --enable-recurrence \
-  --prefix=/var/tmp/synce \
-  --with-libsynce=/var/tmp/synce \
-  --with-librapi2=/var/tmp/synce
+  --prefix=/var/tmp/synce
 make
