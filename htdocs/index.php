41351cb7fc922ea4d8dc178ce1762a1c7102feea
David Eriksson <david@activout.se>
2006-11-06 18:06:00 +0000

    Appeal for help
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2619 599ab11a-0111-0410-8a50-d7a5b0953570

---
+++ htdocs/index.php
@@ -0,0 +1,66 @@
+<?php include 'header.php'; ?>
+
+<div class="PAGE">
+
+<h1>
+
+<a href="http://sourceforge.net"><img
+src="http://sourceforge.net/sflogo.php?group_id=30550&amp;type=5" 
+width="210"
+height="62" border="0" alt="SourceForge Logo" align="right"/></a> 
+
+SynCE is not dead!</h1>
+
+<p><big>But it needs some help. Maybe from you?</big></p>
+
+<h2>Help needed</h2>
+
+<p><big>The SynCE projects needs people that...</big></p>
+
+<ul>
+
+<li>Create a roadmap for SynCE and try to make the project follow 
+it</li>
+
+<li>Handle all bugs in the <a 
+href="http://sourceforge.net/tracker/?group_id=30550&atid=399601">Bug 
+tracker</a></li>
+
+<li>Clean-up the patches for the <tt>ipaq</tt> driver and submit 
+them to the linux-usb mailing list so we can remove the 
+kernel-2.6-driver download.</li>
+
+<li>Reply to people who ask questions on the forums and mailing 
+lists</li>
+
+<li>Improve the GNOME support: synce-gnomevfs, 
+synce-software-manager, synce-trayicon</li>
+
+<li>Improve the OpenSync plugin for synchronizing pre-WM5 
+devices</li>
+
+<li>Make packages for various Linux distributions</li>
+
+<li>Do other things that benefit the project</li>
+
+</ul>
+
+<p>Are you interested or just want to add to the list above? Send 
+a mail to <a href="mailto:twogood@users.sourceforge.net">David 
+Eriksson</a>.</p>
+
+
+<h2>Continue to...</h2>
+
+<ul>
+
+<li><big><a href="http://www.synce.org/">New wiki-based web 
+site</a></big></li>
+<li><a href="index2.php">Old web site</a></li>
+<li><a href="http://sourceforge.net/projects/synce/">SourceForge 
+Project Page</a></li>
+
+</ul>
+
+</div>
+<?php include 'footer.php'; ?>
