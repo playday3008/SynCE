5ddfcf2e298b94321a0e4c34254e3de47566fa27
David Eriksson <david@activout.se>
2006-11-10 21:20:46 +0000

    No C++ comments in C, please.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2622 599ab11a-0111-0410-8a50-d7a5b0953570

--- libsynce/rebuild.sh
+++ libsynce/rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-export CFLAGS="-Wall -Werror -ggdb3"
+export CFLAGS="-Wall -Werror -ggdb3 -pedantic-errors -D_FORTIFY_SOURCE=2"
 ./bootstrap &&
 ./configure --prefix=/var/tmp/synce &&
 make install
