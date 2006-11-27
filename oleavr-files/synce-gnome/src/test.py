359b0754818b188d68feb746b40c25f8630d00db
Ole André Vadla Ravnås <oleavr@gmail.com>
2006-11-27 23:27:06 +0000

    Show notifications.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2687 599ab11a-0111-0410-8a50-d7a5b0953570

--- oleavr-files/synce-gnome/src/test.py
+++ oleavr-files/synce-gnome/src/test.py
@@ -5,18 +5,25 @@ import gobject
 import dbus
 import dbus.glib
 
-bus = dbus.SessionBus()
-proxy_obj = bus.get_object("org.synce.vdccm.EventManager", "/org/synce/vdccm/EventManager")
-mgr = dbus.Interface(proxy_obj, "org.synce.vdccm.EventManager")
+class TestApp:
+    def __init__(self):
+        bus = dbus.SessionBus()
+        proxy_obj = bus.get_object("org.synce.vdccm.EventManager", "/org/synce/vdccm/EventManager")
+        mgr = dbus.Interface(proxy_obj, "org.synce.vdccm.EventManager")
+        mgr.connect_to_signal("DeviceConnected", self.device_connected_cb)
+        mgr.connect_to_signal("DeviceDisconnected", self.device_disconnected_cb)
 
-def device_connected_cb(name):
-    print "device_connected: '%s'" % name
+        notif_obj = bus.get_object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
+        self.notify_iface = dbus.Interface(notif_obj, "org.freedesktop.Notifications")
+        
+    def device_connected_cb(self, name):
+        print "device_connected: '%s'" % name
+        self.notify_iface.Notify("SynCE", 0, "", "PDA connected", "The PDA '%s' just connected." % name, [], {}, 3000)
 
-def device_disconnected_cb(name):
-    print "device_disconnected: '%s'" % name
+    def device_disconnected_cb(self, name):
+        print "device_disconnected: '%s'" % name
+        self.notify_iface.Notify("SynCE", 0, "", "PDA disconnected", "The PDA '%s' just disconnected." % name, [], {}, 3000)
 
-mgr.connect_to_signal("DeviceConnected", device_connected_cb)
-mgr.connect_to_signal("DeviceDisconnected", device_disconnected_cb)
-
+TestApp()
 mainloop = gobject.MainLoop()
 mainloop.run()
