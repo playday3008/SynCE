248489a4214ffc9099f20c0a63fb1235c5e0cb19
Ole André Vadla Ravnås <oleavr@gmail.com>
2006-12-26 15:09:15 +0000

    Making progress, although currently broken.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2724 599ab11a-0111-0410-8a50-d7a5b0953570

--- device-manager/manager/DTPT.py
+++ device-manager/manager/DTPT.py
@@ -71,7 +71,7 @@ class DTPTServer:
         if len(buf) != payload_size:
             print "_handle_nsp_lookup_begin: read %d bytes, expected %d" % (len(buf), payload_size)
             return
-        #open("/tmp/request.bin", "w").write(buf)
+        open("/tmp/request.bin", "w").write(buf)
         qs = QuerySet()
         qs.initialize(buf)
 
@@ -79,31 +79,30 @@ class DTPTServer:
         print "ConnectionSession"
 
 
-SVCID_INET_HOSTADDRBYNAME = "{0002A803-0000-0000-C000-000000000046}"
-
-class QuerySet:
-    def __init__(self):
-        pass
+class RPCStream:
+    def __init__(self, data=""):
+        self.initialize(data, 0)
 
     def initialize(self, data):
         self._data = data
         self._offset = 0
         
-        raw_flat = self.read_field(60)
-        print "Size: %d" % struct.unpack("<I", raw_flat[0:4])
-        print "ServiceInstanceName: %s" % self.read_string()
-        print "ServiceClassID: %s" % self.read_guid()
-        print "Version: 0x%08x" % struct.unpack("<L", raw_flat[12:16])
-        print "Comment: %s" % self.read_string()
-        print "NameSpace: 0x%08x" % struct.unpack("<L", raw_flat[20:24])
-        print "NSProviderId: %s" % self.read_guid()
-        print "Context: %s" % self.read_string()
-        print "NumberOfProtocols: %d" % struct.unpack("<L", raw_flat[32:36])
+    def get_data(self):
+        return self._data
+    
+    def get_offset(self):
+        return self._offset
+    
+    def get_length(self):
+        return len(self._data)
 
     def read_dword(self):
         dw = struct.unpack("<I", self._data[self._offset:self._offset + 4])[0]
         self._offset += 4
         return dw
+    
+    def write_dword(self, dw):
+        self._data += struct.pack("<I", dw)
     
     def read_n_bytes(self, n):
         buf = self._data[self._offset:self._offset + n]
@@ -114,6 +113,13 @@ class QuerySet:
         self._offset += n + align
         return buf
     
+    def write_bytes(self, data):
+        self._data += data
+        if len(data) % 4 != 0:
+            align = 4 - (len(data) % 4)
+            for i in xrange(align):
+                self._data += "\x00"
+
     def read_field(self, expected_size=-1):
         size = self.read_dword()
         if size == 0:
@@ -122,6 +128,10 @@ class QuerySet:
         if expected_size != -1 and len(data) != expected_size:
             raise ValueError("expected_size=%d, size=%d" % (expected_size, len(data)))
         return data
+    
+    def write_field(self, data):
+        self.write_dword(len(data))
+        self.write_bytes(data)
 
     def read_string(self):
         s = self.read_field()
@@ -129,7 +139,13 @@ class QuerySet:
             return s
         #open("/tmp/woot.bin", "wb").write(s)
         return s.decode("utf_16_le").rstrip("\0")
-    
+
+    def write_string(self, s):
+        if s != None:
+            self.write_field(s.encode("utf_16_le") + "\x00\x00")
+        else:
+            self.write_dword(0)
+
     def read_guid(self):
         data = self.read_field(16)
         if data == None:
@@ -140,6 +156,131 @@ class QuerySet:
             (v1, v2, v3, ord(data[8]), ord(data[9]), ord(data[10]),
              ord(data[11]), ord(data[12]), ord(data[13]), ord(data[14]),
              ord(data[15]))
+
+    def write_guid(self, guid):
+        if guid != None:
+            data = struct.pack("<LHHBBBBBBBB",
+                               int(guid[1:9], 16),
+                               int(guid[10:14], 16),
+                               int(guid[15:19], 16),
+                               int(guid[20:22], 16),
+                               int(guid[22:24], 16),
+                               int(guid[24:26], 16),
+                               int(guid[26:28], 16),
+                               int(guid[28:30], 16),
+                               int(guid[30:32], 16),
+                               int(guid[32:34], 16),
+                               int(guid[34:36], 16))
+            self.write_field(data)
+        else:
+            self.write_dword(0)
+
+
+SVCID_INET_HOSTADDRBYNAME = "{0002A803-0000-0000-C000-000000000046}"
+
+class QuerySet:
+    def __init__(self):
+        self.service_instance_name = None
+        self.service_class_id = None
+        self.version = None
+        self.comment = None
+        self.namespace = None
+        self.ns_provider_id = None
+        self.context = None
+        self.num_protocols = 0
+        self.query_string = None
+        self.num_cs_addrs = 0
+        self.output_flags = 0
+        self.blob_size = 0
+
+    def unserialize(self, data):
+        s = RPCStream(data)
+
+        raw_flat = s.read_field(60)
+        size = struct.unpack("<I", raw_flat[0:4])[0]
+        assert size == 60
+
+        self.service_instance_name = s.read_string()
+        self.service_class_id = s.read_guid()
+        self.version = struct.unpack("<L", raw_flat[12:16])[0]
+        self.comment = s.read_string()
+        self.namespace = struct.unpack("<L", raw_flat[20:24])[0]
+        self.ns_provider_id = s.read_guid()
+        self.context = s.read_string()
+        self.num_protocols = s.read_dword()
+        if self.num_protocols > 0:
+            print "FIXME1"
+        self.query_string = s.read_string()
+        self.num_cs_addrs = s.read_dword()
+        if self.num_cs_addrs > 0:
+            print "FIXME2"
+        self.output_flags = struct.unpack("<L", raw_flat[52:56])[0]
+        self.blob_size = s.read_dword()
+        
+        assert s.get_offset() == s.get_length()
+
+    def serialize(self):
+        s = RPCStream()
+
+        dummy_ptr = 0xDEADBEEF
+
+        if self.service_instance_name != None: svc_name_ptr = dummy_ptr
+        else: svc_name_ptr = 0
+
+        if self.service_class_id != None: svc_cls_id_ptr = dummy_ptr
+        else: svc_cls_id_ptr = 0
+        
+        if self.comment != None: comment_ptr = dummy_ptr
+        else: comment_ptr = 0
+
+        if self.ns_provider_id != None: ns_provider_id_ptr = dummy_ptr
+        else: ns_provider_id_ptr = 0
+
+        if self.context != None: context_ptr = dummy_ptr
+        else: context_ptr = 0
+
+        if self.num_protocols > 0: protocols_ptr = dummy_ptr
+        else: protocols_ptr = 0
+
+        if self.query_string != None: query_string_ptr = dummy_ptr
+        else: query_string_ptr = 0
+
+        if self.num_cs_addrs > 0: cs_addrs_ptr = dummy_ptr
+        else: cs_addrs_ptr = 0
+
+        if self.blob_size > 0: blob_ptr = dummy_ptr
+        else: blob_ptr = 0
+
+        buf = struct.pack("<LLLLLLLLLLLLLLL",
+                          60,
+                          svc_name_ptr,
+                          svc_cls_id_ptr,
+                          self.version,
+                          comment_ptr,
+                          self.namespace,
+                          ns_provider_id_ptr,
+                          context_ptr,
+                          self.num_protocols,
+                          protocols_ptr,
+                          query_string_ptr,
+                          self.num_cs_addrs,
+                          cs_addrs_ptr,
+                          self.output_flags,
+                          blob_ptr)
+        s.write_bytes(buf)
+
+        s.write_string(self.service_instance_name)
+        s.write_guid(self.service_class_id)
+        s.write_string(self.comment)
+        s.write_guid(self.ns_provider_id)
+        s.write_string(self.context)
+        s.write_dword(0) # FIXME: WSAQUERYSET.dwNumberOfProtocols
+        s.write_string(self.query_string)
+        s.write_dword(0) # FIXME: WSAQUERYSET.dwNumberOfCsAddrs
+        s.write_dword(0) # FIXME: WSAQUERYSET.lpBlob
+
+        return s.get_data()
+
 
 if __name__ == "__main__":
     srv = DTPTServer()
