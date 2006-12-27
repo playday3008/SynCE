a4b36904400421c851cca6bfbdd6161ceb7fd8f0
Ole André Vadla Ravnås <oleavr@gmail.com>
2006-12-27 02:36:28 +0000

    NSPSession implemented! DNS-lookups are working :-D
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2726 599ab11a-0111-0410-8a50-d7a5b0953570

--- device-manager/manager/DTPT.py
+++ device-manager/manager/DTPT.py
@@ -20,12 +20,19 @@
 import socket
 import thread
 import struct
+import random
 
 DTPT_PORT = 5721
+DUMMY_PTR = 0xDEADBEEF
+SVCID_INET_HOSTADDRBYNAME = "{0002A803-0000-0000-C000-000000000046}"
+
+WSAEFAULT = 10014
+WSAHOST_NOT_FOUND = 11001
 
 class DTPTServer:
     def __init__(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(("0.0.0.0", DTPT_PORT))
         s.listen(5)
         self._sock = s
@@ -37,50 +44,124 @@ class DTPTServer:
             thread.start_new_thread(self._handle_client, (s,))
     
     def _handle_client(self, s):
-        print "handling client"
-        try:
-            buf = s.recv(2, socket.MSG_PEEK | socket.MSG_WAITALL)
-            print "%02x %02x" % (ord(buf[0]), ord(buf[1]))
-            if ord(buf[0]) != 1:
-                print "invalid protocol"
-            if ord(buf[1]) == 9:
-                self._handle_nsp_session(s)
-            elif ord(buf[1]) == 1:
-                self._handle_connection_session(s)
-            else:
-                print "unknown session type"
+        print "Handling client"
+        buf = s.recv(2, socket.MSG_PEEK | socket.MSG_WAITALL)
+        if ord(buf[0]) != 1:
+            print "Invalid protocol"
             return
-        except Exception, e:
-            print dir(e)
-            print e
+        if ord(buf[1]) == 9:
+            s = NSPSession(s)
+            s.run()
+        elif ord(buf[1]) == 1:
+            self._handle_connection_session(s)
+        else:
+            print "unknown session type"
 
-    def _handle_nsp_session(self, s):
-        print "NSPSession"
-        while self._running:
-            buf = s.recv(20, socket.MSG_WAITALL)
+    def _handle_connection_session(self, s):
+        print "ConnectionSession"
+
+
+class NSPSession:
+    def __init__(self, s):
+        self.socket = s
+        self.initialize()
+        
+    def initialize(self):
+        self.handle = None
+        self.request = None
+        self.response = None
+
+    def run(self):
+        while True:
+            buf = self.socket.recv(20, socket.MSG_WAITALL)
             if len(buf) != 20:
-                print "_handle_nsp_session: read %d bytes, expected %d" % (len(buf), 20)
+                if len(buf) == 0:
+                    print "NSPSession: client disconnected"
+                else:
+                    print "NSPSession: read %d bytes, expected %d" % (len(buf), 20)
+                return
+
             ver, type, qv, dv1, dv2 = struct.unpack("<BBxxQII", buf)
             if ver != 1:
                 raise ValueError("ver=%d, expected 1" % ver)
             
             if type == 9:
-                self._handle_nsp_lookup_begin(s, qv, dv1, dv2)
+                print "NSPSession: handling LookupBegin"
+                self._handle_lookup_begin(qv, dv1, dv2)
+            elif type == 11:
+                print "NSPSession: handling LookupNext"
+                self._handle_lookup_next(qv, dv1, dv2)
+            elif type == 13:
+                print "NSPSession: handling LookupEnd"
+                self.initialize()
             else:
-                print "unhandled type=%d" % type
+                print "NSPSession: unhandled type=%d" % type
 
-    def _handle_nsp_lookup_begin(self, s, q_value, control_flags, payload_size):
-        print "_handle_nsp_lookup_begin: flags=0x%08x, payloadSize=%d" % (control_flags, payload_size)
-        buf = s.recv(payload_size, socket.MSG_WAITALL)
+    def _handle_lookup_begin(self, q_value, control_flags, payload_size):
+        buf = self.socket.recv(payload_size, socket.MSG_WAITALL)
         if len(buf) != payload_size:
             print "_handle_nsp_lookup_begin: read %d bytes, expected %d" % (len(buf), payload_size)
             return
-        open("/tmp/request.bin", "w").write(buf)
+        
+        if self.handle != None:
+            print "_handle_nsp_lookup_begin: called twice"
+            self.socket.close()
+            return
+
         qs = QuerySet()
         qs.unserialize(buf)
+        #print qs
+        
+        if qs.service_class_id != SVCID_INET_HOSTADDRBYNAME:
+            print "Unsupported ServiceClassId %s" % qs.service_class_id
+            # FIXME: return a proper error here
+            self._send_reply(10, 0, 0xDEADBEEF, 0)
+            return
 
-    def _handle_connection_session(self, s):
-        print "ConnectionSession"
+        self.handle = random.randint(2**10, 2**31)
+        self.request = qs
+
+        self._send_reply(10, self.handle, 0, 0)
+
+    def _handle_lookup_next(self, handle, dvalue1, buffer_size):
+        if self.response == None:
+            qs = QuerySet()
+            qs.service_instance_name = self.request.service_instance_name
+            qs.namespace = 12
+
+            try:
+                results = socket.getaddrinfo(qs.service_instance_name, None)
+            except:
+                self._send_reply(12, 0, WSAHOST_NOT_FOUND, 0)
+                return
+            
+            for result in results:
+                family, socket_type, proto, canonname, sockaddr = result
+                
+                ai = CSAddrInfo()
+                ai.local_addr = (family, "0.0.0.0", 0)
+                ai.remote_addr = (family, sockaddr[0], sockaddr[1])
+                ai.socket_type = socket_type
+                ai.protocol = proto
+                
+                qs.cs_addrs.append(ai)
+            
+            self.response = qs.serialize()
+            
+            #print "Prepared response (%d bytes):" % len(self.response)
+            #print qs
+
+        if len(self.response) <= buffer_size:
+            self._send_reply(12, 0, 0, len(self.response))
+            self.socket.sendall(self.response)
+        else:
+            self._send_reply(12, 0, WSAEFAULT, len(self.response))
+
+    def _send_reply(self, msg_type, qvalue, dvalue1, dvalue2):
+        print "NSPSession: sending reply, msg_type=%d, qvalue=%d, dvalue1=%d, dvalue2=%d" \
+            % (msg_type, qvalue, dvalue1, dvalue2)
+        buf = struct.pack("<BBxxQII", 1, msg_type, qvalue, dvalue1, dvalue2)
+        self.socket.sendall(buf)
 
 
 class RPCStream:
@@ -180,20 +261,18 @@ class RPCStream:
             self.write_dword(0)
 
 
-SVCID_INET_HOSTADDRBYNAME = "{0002A803-0000-0000-C000-000000000046}"
-
 class QuerySet:
     def __init__(self):
         self.service_instance_name = None
         self.service_class_id = None
-        self.version = None
+        self.version = 0
         self.comment = None
         self.namespace = None
         self.ns_provider_id = None
         self.context = None
         self.num_protocols = 0
         self.query_string = None
-        self.num_cs_addrs = 0
+        self.cs_addrs = []
         self.output_flags = 0
         self.blob_size = 0
 
@@ -215,9 +294,21 @@ class QuerySet:
         if self.num_protocols > 0:
             print "FIXME1"
         self.query_string = s.read_string()
-        self.num_cs_addrs = s.read_dword()
-        if self.num_cs_addrs > 0:
-            print "FIXME2"
+        
+        self.cs_addrs = []
+        count = s.read_dword()
+        if count > 0:
+            data = s.read_field(count * 24)
+            offset = 0
+            for i in xrange(len(data) / 24):
+                local_data = s.read_field()
+                remote_data = s.read_field()
+
+                ai = CSAddrInfo()
+                ai.unserialize(data[offset:offset + 24], local_data, remote_data)
+                self.cs_addrs.append(ai)
+                offset += 24
+
         self.output_flags = struct.unpack("<L", raw_flat[52:56])[0]
         self.blob_size = s.read_dword()
 
@@ -226,33 +317,31 @@ class QuerySet:
     def serialize(self):
         s = RPCStream()
 
-        dummy_ptr = 0xDEADBEEF
-
-        if self.service_instance_name != None: svc_name_ptr = dummy_ptr
+        if self.service_instance_name != None: svc_name_ptr = DUMMY_PTR
         else: svc_name_ptr = 0
 
-        if self.service_class_id != None: svc_cls_id_ptr = dummy_ptr
+        if self.service_class_id != None: svc_cls_id_ptr = DUMMY_PTR
         else: svc_cls_id_ptr = 0
         
-        if self.comment != None: comment_ptr = dummy_ptr
+        if self.comment != None: comment_ptr = DUMMY_PTR
         else: comment_ptr = 0
 
-        if self.ns_provider_id != None: ns_provider_id_ptr = dummy_ptr
+        if self.ns_provider_id != None: ns_provider_id_ptr = DUMMY_PTR
         else: ns_provider_id_ptr = 0
 
-        if self.context != None: context_ptr = dummy_ptr
+        if self.context != None: context_ptr = DUMMY_PTR
         else: context_ptr = 0
 
-        if self.num_protocols > 0: protocols_ptr = dummy_ptr
+        if self.num_protocols > 0: protocols_ptr = DUMMY_PTR
         else: protocols_ptr = 0
 
-        if self.query_string != None: query_string_ptr = dummy_ptr
+        if self.query_string != None: query_string_ptr = DUMMY_PTR
         else: query_string_ptr = 0
 
-        if self.num_cs_addrs > 0: cs_addrs_ptr = dummy_ptr
+        if len(self.cs_addrs) > 0: cs_addrs_ptr = DUMMY_PTR
         else: cs_addrs_ptr = 0
 
-        if self.blob_size > 0: blob_ptr = dummy_ptr
+        if self.blob_size > 0: blob_ptr = DUMMY_PTR
         else: blob_ptr = 0
 
         buf = struct.pack("<LLLLLLLLLLLLLLL",
@@ -267,11 +356,11 @@ class QuerySet:
                           self.num_protocols,
                           protocols_ptr,
                           query_string_ptr,
-                          self.num_cs_addrs,
+                          len(self.cs_addrs),
                           cs_addrs_ptr,
                           self.output_flags,
                           blob_ptr)
-        s.write_bytes(buf)
+        s.write_field(buf)
 
         s.write_string(self.service_instance_name)
         s.write_guid(self.service_class_id)
@@ -280,12 +369,112 @@ class QuerySet:
         s.write_string(self.context)
         s.write_dword(0) # FIXME: WSAQUERYSET.dwNumberOfProtocols
         s.write_string(self.query_string)
-        s.write_dword(0) # FIXME: WSAQUERYSET.dwNumberOfCsAddrs
+        
+        s.write_dword(len(self.cs_addrs))
+        buf = ""
+        addrs = []
+        for addr in self.cs_addrs:
+            data, local_data, remote_data = addr.serialize()
+            buf += data
+            addrs.append(local_data)
+            addrs.append(remote_data)
+        s.write_field(buf)
+        
+        for addr in addrs:
+            s.write_field(addr)
+        
         s.write_dword(0) # FIXME: WSAQUERYSET.lpBlob
 
         return s.get_data()
 
+    def __str__(self):
+        addrs = ""
+        for addr in self.cs_addrs:
+            addrs += "\n    %s" % str(addr)
+        
+        body_templ = """<QuerySet>
+  <ServiceInstanceName>%s</ServiceInstanceName>
+  <ServiceClassId>%s</ServiceClassId>
+  <Version>0x%08x</Version>
+  <Comment>%s</Comment>
+  <NameSpace>%s</NameSpace>
+  <NSProviderId>%s</NSProviderId>
+  <Context>%s</Context>
+  <Protocols/>
+  <QueryString>%s</QueryString>
+  <CsAddrs>%s
+  </CsAddrs>
+  <OutputFlags>0x%08x</OutputFlags>
+  <Blob/>
+</QuerySet>"""
+
+        return body_templ % \
+            (self.service_instance_name, self.service_class_id,
+             self.version, self.comment, self.namespace,
+             self.ns_provider_id, self.context, self.query_string,
+             addrs, self.output_flags)
+
+
+class CSAddrInfo:
+    def __init__(self):
+        self.local_addr = None
+        self.remote_addr = None
+        self.socket_type = -1
+        self.protocol = -1
+
+    def unserialize(self, data, local_data, remote_data):
+        self.socket_type, self.protocol = struct.unpack("<LL", data[16:24])
+        self.local_addr = self._unserialize_sockaddr(local_data)
+        self.remote_addr = self._unserialize_sockaddr(remote_data)
+        
+    def serialize(self):
+        local_data = self._serialize_sockaddr(self.local_addr)
+        remote_data = self._serialize_sockaddr(self.remote_addr)
+
+        if self.local_addr != None: local_addr_ptr = DUMMY_PTR
+        else: local_addr_ptr = 0
+
+        if self.remote_addr != None: remote_addr_ptr = DUMMY_PTR
+        else: remote_addr_ptr = 0
+        
+        return (struct.pack("<LLLLLL", local_addr_ptr, len(local_data),
+                            remote_addr_ptr, len(remote_data),
+                            self.socket_type, self.protocol),
+                local_data, remote_data)
+
+    def _unserialize_sockaddr(self, data):
+        family = struct.unpack("<H", data[0:2])[0]
+        if family == socket.AF_INET:
+            port = struct.unpack(">H", data[2:4])[0]
+            return (family, socket.inet_ntoa(data[4:8]), port)
+
+    def _serialize_sockaddr(self, addr):
+        if addr == None:
+            return None
+        
+        family, ascii_addr, port = addr
+        
+        s = struct.pack("<H", family)
+        s += struct.pack(">H", port)
+        s += socket.inet_aton(ascii_addr)
+        s += "\x00\x00\x00\x00\x00\x00\x00\x00"
+
+        return s
+
+    def __str__(self):
+        return "<CSAddrInfo><LocalAddress>%s</LocalAddress><RemoteAddress>%s</RemoteAddress><SocketType>%d</SocketType><Protocol>%d</Protocol></CSAddrInfo>" % \
+            (self.local_addr, self.remote_addr, self.socket_type, self.protocol)
+
+def test():
+    req = QuerySet()
+    req.unserialize(open("/home/oleavr/incoming/request.bin", "rb").read())
+    resp = QuerySet()
+    resp.unserialize(open("/home/oleavr/incoming/response.bin", "rb").read())
+    
+    print req
+    print resp
 
 if __name__ == "__main__":
+    #test()
     srv = DTPTServer()
     srv.run()
