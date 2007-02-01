baa24445d19ba07ba41f512b024dca48643141b0
Rich Alimi <rich@velvetsea.net>
2007-02-01 10:39:15 +0000

    Use new format conversion calls
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2743 599ab11a-0111-0410-8a50-d7a5b0953570

--- sync-engine/tests/contact_air2os.py
+++ sync-engine/tests/contact_air2os.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import logging
 sys.path.insert(0, "..")
 from engine import formats
 from xml.dom import minidom
 
+logging.basicConfig(level=logging.DEBUG,
+                    stream=sys.stdout,
+                    format='%(asctime)s %(levelname)s %(name)s : %(message)s')
+
 src = minidom.parseString(sys.stdin.read())
-dst = formats.contact.from_airsync("uid", src.documentElement)
+dst = formats.contact.from_airsync(src.documentElement)
 print dst.toprettyxml(encoding="utf-8")
