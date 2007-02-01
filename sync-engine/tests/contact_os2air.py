ad841b4d7be821cd6138b18c5b5c135f33d4a0ea
Rich Alimi <rich@velvetsea.net>
2007-02-01 10:35:57 +0000

    Added logging system configuration
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@2741 599ab11a-0111-0410-8a50-d7a5b0953570

--- sync-engine/tests/contact_os2air.py
+++ sync-engine/tests/contact_os2air.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import logging
 sys.path.insert(0, "..")
 from engine import formats
 from xml.dom import minidom
+
+logging.basicConfig(level=logging.DEBUG,
+                    stream=sys.stdout,
+                    format='%(asctime)s %(levelname)s %(name)s : %(message)s')
 
 src = minidom.parseString(sys.stdin.read())
 dst = formats.contact.to_airsync(src)
