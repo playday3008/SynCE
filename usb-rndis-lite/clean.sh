19948ef3476031e080d60ba98bff72daa75a5397
Jc2k <john.carr@unrouted.co.uk>
2007-11-16 10:33:02 +0000

    ./clean.sh doesn't error now - it only scares users otherwise
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3049 599ab11a-0111-0410-8a50-d7a5b0953570

--- usb-rndis-lite/clean.sh
+++ usb-rndis-lite/clean.sh
@@ -4,4 +4,6 @@ find /lib/modules/$(uname -r) -name "usbnet.ko" -exec rm -v {} \;
 find /lib/modules/$(uname -r) -name "cdc_ether.ko" -exec rm -v {} \;
 find /lib/modules/$(uname -r) -name "rndis_host.ko" -exec rm -v {} \;
 depmod -ae
-./reload.sh --unload-only
+./reload.sh --unload-only 2> /dev/null
+
+echo "Done! :)"
