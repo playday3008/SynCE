0e0b19f9ae810b3e8948f53e81c712665b026f03
Jc2k <john.carr@unrouted.co.uk>
2007-12-16 16:28:08 +0000

    Use autogen.sh instead of bootstrap so we can use jhbuild easily
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3083 599ab11a-0111-0410-8a50-d7a5b0953570

--- rra/rebuild.sh
+++ rra/rebuild.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 set -e
 ./cvsclean 
-./bootstrap 
 export CFLAGS="-Wall -Werror"
 export PKG_CONFIG_PATH="/var/tmp/synce/lib/pkgconfig:$PKG_CONFIG_PATH"
-./configure \
+./autogen.sh \
   --enable-recurrence \
   --prefix=/var/tmp/synce
 make
