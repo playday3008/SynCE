5c12fd48af19645fa0ba39d1dee5563c68e84ddf
Jc2k <john.carr@unrouted.co.uk>
2007-12-16 16:26:02 +0000

    Use autogen.sh instead of bootstrap so we can use jhbuild easily
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3082 599ab11a-0111-0410-8a50-d7a5b0953570

--- librapi2/rebuild.sh
+++ librapi2/rebuild.sh
@@ -2,7 +2,6 @@
 set -x
 export CFLAGS=-Werror
 export CXXFLAGS=-Werror
-export PKG_CONFIG_PATH="/var/tmp/synce/lib/pkgconfig:$PKG_CONFIG_PATH
-./bootstrap &&
-./configure --prefix=/var/tmp/synce &&
+export PKG_CONFIG_PATH="/var/tmp/synce/lib/pkgconfig:$PKG_CONFIG_PATH"
+./autogen.sh --prefix=/var/tmp/synce &&
 make install
