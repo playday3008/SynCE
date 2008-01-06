8bbf842146eb8d40e3f52e07f3982700b38de0e4
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-01-06 14:07:02 +0000

    Fixed up setup.py to install the OpenSync plugin into the right directory, or fail with a message if the directory cannot be worked out.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3146 599ab11a-0111-0410-8a50-d7a5b0953570

--- sync-engine/setup.py
+++ sync-engine/setup.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import commands
+import sys
+
+(exitstatus, plugindir) = commands.getstatusoutput('pkg-config opensync-1.0 --variable=plugindir')
+if plugindir == '':
+    if exitstatus == 0:
+        print 'Could not get OpenSync plugin directory, even though OpenSync is installed..'
+    else:
+        print 'Could not get OpenSync plugin directory; is the OpenSync dev package installed?'
+    sys.exit(1)
+elif exitstatus == 32512:
+    print 'Could not get OpenSync plugin directory; pkg-config is not installed.'
+    sys.exit(1)
 
 setup(
     name = "SyncEngine",
@@ -15,5 +28,8 @@ setup(
     package_data = {
         '': ['*.xsl'],
     },
+    data_files = [
+        (plugindir+'/python-plugins', ['opensync-plugin-0.30later.py', 'opensync-plugin.py'])
+    ]
 )
 
