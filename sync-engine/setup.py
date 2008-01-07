db1deee1e4f1b051feda08cd423e183898aafc11
Dr J A Gow <jgow@dmu.ac.uk>
2008-01-07 22:11:29 +0000

    Prevent setup.py from failing.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3167 599ab11a-0111-0410-8a50-d7a5b0953570

--- sync-engine/setup.py
+++ sync-engine/setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from synce_install_plugins import get_plugindir, get_opensync_version
 
 if get_opensync_version() >= 0.30:
     pluginfile = ['opensync-plugin-0.30later.py']
@@ -20,13 +19,9 @@ setup(
                'tools/delete_partnership.py',
                'tools/list_partnerships.py',
                'tools/configure_bindings.py',
-               'synce_install_plugins.py'],
+               'synce-install-plugins.py'],
     package_data = {
         '': ['*.xsl'],
-    },
-    data_files = [
-        (get_plugindir()+'/python-plugins', pluginfile),
-        ('/usr/share/sync-engine', otherplugin)
-    ]
+    }
 )
 
