bfcc103ca9c219dcc1c46aab65adc10c0c6837db
Mark Ellis <mark@mpellis.org.uk>
2008-04-16 15:39:59 +0000

    * check for orphaned WM6 items when deleting partnership * if delete partnership selected for a sync item, get the parent * fix pointer signedness warning in vdccm gtop
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3413 599ab11a-0111-0410-8a50-d7a5b0953570

--- trayicon/src/gtop_stuff.c
+++ trayicon/src/gtop_stuff.c
@@ -63,12 +63,12 @@ get_process_name (gchar *cmd, gchar *args)
 static GArray *
 find_dccm () {
 	glibtop_proclist proclist;
-	unsigned *pid_list;
+	pid_t *pid_list;
 	gint i = 0;
 	gint n = 0;
 	gint which, arg;
 
-	GArray *dccm_list = g_array_new(FALSE, FALSE, sizeof(unsigned));
+	GArray *dccm_list = g_array_new(FALSE, FALSE, sizeof(pid_t));
 
 	glibtop_init ();
 	
@@ -134,7 +134,7 @@ send_signal_dccm(int sig) {
   num = 0;
   while (num < dccm_list->len)
     {
-      if (kill(g_array_index(dccm_list, unsigned, num), sig) != 0)
+      if (kill(g_array_index(dccm_list, pid_t, num), sig) != 0)
 	result = FALSE;
       num++;
     }
