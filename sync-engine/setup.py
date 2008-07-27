22e616e82afb0999d8b42b48641ab105443376ca
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-07-27 17:41:15 +0000

    Upped version number.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3541 599ab11a-0111-0410-8a50-d7a5b0953570

--- sync-engine/setup.py
+++ sync-engine/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "sync-engine",
-    version = "0.12",
+    version = "0.13",
     packages = find_packages(),
     scripts = ['sync-engine',
                'tools/clean_partnerships.py',
