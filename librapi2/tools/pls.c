00ed0ed8606387e601452d3621ed447833d31fd6
Mark Ellis <mark@mpellis.org.uk>
2008-08-11 20:27:48 +0000

    option for recursive listing in pls
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3545 599ab11a-0111-0410-8a50-d7a5b0953570

--- librapi2/tools/pls.c
+++ librapi2/tools/pls.c
@@ -11,15 +11,18 @@ char* dev_name = NULL;
 
 static bool numeric_file_attributes = false;
 static bool show_hidden_files = false;
+static bool recursive = false;
+static const char *wildcards = "*.*";
 
 static void show_usage(const char* name)
 {
 	fprintf(stderr,
 			"Syntax:\n"
 			"\n"
-			"\t%s [-a] [-d LEVEL] [-p DEVNAME] [-h] [-n] [DIRECTORY]\n"
+			"\t%s [-a] [-R] [-d LEVEL] [-p DEVNAME] [-h] [-n] [DIRECTORY]\n"
 			"\n"
 			"\t-a        Show all files including those marked as hidden\n"
+			"\t-R        Recursively display directories\n"
 			"\t-d LEVEL  Set debug log level\n"
 			"\t              0 - No logging (default)\n"
 			"\t              1 - Errors only\n"
@@ -28,7 +31,7 @@ static void show_usage(const char* name)
 			"\t-h         Show this help message\n"
                         "\t-p DEVNAME Mobile device name\n"
 			"\t-n         Show numeric value for file attributes\n"
-			"\tDIRECTORY  The remote directory where you want to list files\n",
+			"\tPATH       The remote path you want to list\n",
 			name);
 }
 
@@ -37,12 +40,16 @@ static bool handle_parameters(int argc, char** argv, char** path)
 	int c;
 	int log_level = SYNCE_LOG_LEVEL_LOWEST;
 
-	while ((c = getopt(argc, argv, "ad:p:hn")) != -1)
+	while ((c = getopt(argc, argv, "aRd:p:hn")) != -1)
 	{
 		switch (c)
 		{
 			case 'a':
 				show_hidden_files = true;
+				break;
+					
+			case 'R':
+				recursive = true;
 				break;
 					
                         case 'p':
@@ -72,6 +79,7 @@ static bool handle_parameters(int argc, char** argv, char** path)
 
 	return true;
 }
+
 
 static void print_attribute(CE_FIND_DATA* entry, DWORD attribute, int c)
 {
@@ -170,7 +178,70 @@ static bool print_entry(CE_FIND_DATA* entry)
 	return true;
 }
 
-static bool list_matching_files(WCHAR* wide_path)
+char *
+absolutize_path(const char *path)
+{
+	WCHAR path_w[MAX_PATH];
+	char *tmp_path1 = NULL;
+	char *tmp_path2 = NULL;
+
+	if ('\\' == path[0])
+		return strdup(path);
+
+	/* if not an absolute path, append to "My Documents" */
+
+	if (!CeGetSpecialFolderPath(CSIDL_PERSONAL, (MAX_PATH * sizeof(WCHAR)), path_w))
+	{
+		fprintf(stderr, "Unable to get the \"My Documents\" path.\n");
+		return NULL;
+	}
+
+	tmp_path1 = wstr_to_current(path_w);
+	tmp_path2 = malloc(strlen(tmp_path1) + strlen(path) + 2);
+	snprintf(tmp_path2, strlen(tmp_path1) + strlen(path) + 2, "%s\\%s", tmp_path1, path);
+	free(tmp_path1);
+	return tmp_path2;
+}
+
+char *
+dirname(const char *path)
+{
+	int dir_end = 0, i;
+	bool wildcard = FALSE;
+	char *tmp_path = NULL;
+
+	/* if no wildcard, return whole path
+	   if wildcard, return path up to last backslash, or empty string if none
+	*/
+
+	i = strlen(path) - 1;
+
+	while (i >= 0)
+	{
+		if ( (path[i] == '*') || (path[i] == '?') ) {
+			wildcard = true;
+			dir_end = 0;
+		}
+
+		if ( (path[i] == '\\') && (dir_end == 0) ) {
+			dir_end = i;
+		}
+
+		i--;
+	}
+
+	if (!wildcard)
+		return strdup(path);
+
+	if (dir_end == 0)
+		return strdup("");
+
+	tmp_path = malloc(dir_end + 1);
+	snprintf(tmp_path, dir_end +1, "%s", path);
+	return tmp_path;
+}
+
+static bool list_matching_files(const char* path, bool first_pass)
 {
 	bool success = false;
 	BOOL result;
@@ -179,7 +250,17 @@ static bool list_matching_files(WCHAR* wide_path)
 	unsigned i;
 	HRESULT hr;
 	DWORD last_error;
+	char *full_path = NULL;
+	char *base_path = NULL;
+	char *new_path = NULL;
+	WCHAR *wide_path = NULL;
+	char *entry_name = NULL;
 
+	if (!(full_path = absolutize_path(path)))
+	  return FALSE;
+
+	wide_path = wstr_from_current(full_path);
+	free(full_path);
 	synce_trace_wstr(wide_path);
 
 	result = CeFindAllFiles(
@@ -187,27 +268,69 @@ static bool list_matching_files(WCHAR* wide_path)
 			(show_hidden_files ? 0 : FAF_ATTRIB_NO_HIDDEN) |
 		 	FAF_ATTRIBUTES|FAF_LASTWRITE_TIME|FAF_NAME|FAF_SIZE_LOW|FAF_OID,
 			&file_count, &find_data);
+	wstr_free_string(wide_path);
 
 	if (!result) {
 		if (FAILED(hr = CeRapiGetError())) {
 		  fprintf(stderr, "Error finding files: %08x: %s.\n",
 			  hr, synce_strerror(hr));
-		  goto exit;
+		  return false;
 		}
 
 		last_error = CeGetLastError();
 		fprintf(stderr, "Error finding files: %d: %s.\n",
 			last_error, synce_strerror(last_error));
-		goto exit;
+		return false;
 	}
 
-	if (file_count == 0) {
+	if ((file_count == 1) && (find_data->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) && first_pass) {
+
+		base_path = dirname(path);
+
+		entry_name = wstr_to_current(find_data->cFileName);
+
+		new_path = malloc(strlen(base_path) + strlen(entry_name) + 6);
+		snprintf(new_path, strlen(base_path) + strlen(entry_name) + 6, "%s\\%s\\%s", base_path, entry_name, wildcards);
+		free(base_path);
+		free(entry_name);
+
+		success = list_matching_files(new_path, FALSE);
+
+		free(new_path);
+		return success;
+	}
+	
+	if ((file_count == 0) && first_pass) {
 		fprintf(stderr, "No such file or directory\n");
 		goto exit;
 	}
-	
+
 	for (i = 0; i < file_count; i++)
 		print_entry(find_data + i);
+
+	if (recursive)
+		printf("Total %d\n", file_count);
+
+	if (recursive) {
+		for (i = 0; i < file_count; i++) {
+
+			if ((find_data + i)->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+			{
+				entry_name = wstr_to_current((find_data + i)->cFileName);
+				printf("\n%s:\n", entry_name);
+
+				base_path = dirname(path);
+
+				new_path = malloc(strlen(base_path) + strlen(entry_name) + 6);
+				snprintf(new_path, strlen(base_path) + strlen(entry_name) + 6, "%s\\%s\\%s", base_path, entry_name, wildcards);
+				free(base_path);
+				free(entry_name);
+
+				list_matching_files(new_path, FALSE);
+				free(new_path);
+			}
+		}
+	}
 	
 	success = true;
 
@@ -217,34 +340,13 @@ exit:
 	return success;
 }
 
-static const char *wildcards = "*.*";
-static       WCHAR empty[]   = {'\0'};
-
-bool list_directory(WCHAR* directory)
-{
-	WCHAR path[MAX_PATH];
-	WCHAR* tmp = NULL;
-
-	if (!directory)
-	{
-		synce_error("Directory is NULL. How did that happen?");
-		return false;
-	}
-
-	synce_trace_wstr(directory);
-	wstrcpy(path, directory);
-	synce_trace_wstr(path);
-	wstr_append(path, (tmp = wstr_from_current(wildcards)), sizeof(path));
-	wstr_free_string(tmp);
-	return list_matching_files(path);
-}
 
 int main(int argc, char** argv)
 {
 	int result = 1;
         RapiConnection* connection = NULL;
 	char* path = NULL;
-	WCHAR* wide_path = NULL;
+	char* tmp_path = NULL;
 	HRESULT hr;
 
 	if (!handle_parameters(argc, argv, &path))
@@ -269,36 +371,20 @@ int main(int argc, char** argv)
 		goto exit;
 	}
 
-	if (path)
-		convert_to_backward_slashes(path);
-
 	if (!path)
-	{
-		wide_path = adjust_remote_path(empty, false);
-		list_directory(wide_path);
+		path = strdup("");
+
+	convert_to_backward_slashes(path);
+
+	if ((strlen(path) > 1) && (path[strlen(path) - 1] == '\\')) {
+		tmp_path = malloc(strlen(path));
+		snprintf(tmp_path, strlen(path), "%s", path);
+		free(path);
+		path = tmp_path;
 	}
-	else if (path[strlen(path)-1] == '\\')
-	{
-		/* This is a directory, append "*" to show its contents */
-		char new_path[MAX_PATH];
-		snprintf(new_path, sizeof(new_path), "%s*", path);
-		wide_path = wstr_from_current(new_path);
-		wide_path = adjust_remote_path(wide_path, true);
 
-		if (!list_matching_files(wide_path))
-			goto exit;
-	}
-	else
-	{
-
-		wide_path = wstr_from_current(path);
-		wide_path = adjust_remote_path(wide_path, true);
-
-		if (!list_matching_files(wide_path))
-			goto exit;
-
-
-	}
+	if (!list_matching_files(path, TRUE))
+		goto exit;
 
 	result = 0;
 
@@ -306,8 +392,7 @@ exit:
 	if (path)
 		free(path);
 
-	wstr_free_string(wide_path);
-
 	CeRapiUninit();
 	return result;
 }
+
