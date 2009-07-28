67d29ee2a36bc9bfc474957a34729daa0f8bdc9c
Mark Ellis <mark@mpellis.org.uk>
2009-07-28 20:59:22 +0000

    patches from Ilya Bakulin to fix compilation problems
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3820 599ab11a-0111-0410-8a50-d7a5b0953570

--- librapi2/src/support/rapi_context.c
+++ librapi2/src/support/rapi_context.c
@@ -513,13 +513,19 @@ HRESULT rapi_context_connect(RapiContext* context)
         /*
          *  odccm, synce-hal, or proxy ?
          */
+#if ENABLE_ODCCM_SUPPORT
         if (strcmp(transport, "odccm") == 0) {
             synce_socket_take_descriptor(context->socket, get_connection_from_odccm(info));
         }
-        else if (strcmp(transport, "hal") == 0) {
+        else
+#endif
+#if ENABLE_HAL_SUPPORT
+        if (strcmp(transport, "hal") == 0) {
             synce_socket_take_descriptor(context->socket, get_connection_from_hal(info));
         }
-        else if ( !synce_socket_connect_proxy(context->socket, synce_info_get_device_ip(info)) )
+        else
+#endif
+	if ( !synce_socket_connect_proxy(context->socket, synce_info_get_device_ip(info)) )
         {
             synce_error("failed to connect to proxy for %s", synce_info_get_device_ip(info));
             goto fail;
