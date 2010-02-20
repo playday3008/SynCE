60743be6228d7c8ec308083f053b7528e4d750bb
Mark Ellis <mark@mpellis.org.uk>
2010-02-20 20:58:31 +0000

    bump version
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@3893 599ab11a-0111-0410-8a50-d7a5b0953570

--- sync-engine/setup.py
+++ sync-engine/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "sync-engine",
-    version = "0.14",
+    version = "0.15",
     packages = find_packages(),
     scripts = ['sync-engine',
                'tools/clean_partnerships.py',
