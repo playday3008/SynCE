3d618de06abd9bc0e3b3003781069d59b9ff616e
Mark Ellis <mark@mpellis.org.uk>
2011-11-16 13:50:50 +0000

    remove obsolete file
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@4019 599ab11a-0111-0410-8a50-d7a5b0953570

--- librra/rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-./cvsclean 
-export CFLAGS="-Wall -Werror"
-export PKG_CONFIG_PATH="/var/tmp/synce/lib/pkgconfig:$PKG_CONFIG_PATH"
-./autogen.sh \
-  --enable-recurrence \
-  --prefix=/var/tmp/synce
-make
