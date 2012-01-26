20044c1b13a3e54a78a616063c05d408fe090cfc
David Eriksson <david@activout.se>
2012-01-26 21:44:33 +0000

    Unshield has moved to github. Please visit https://github.com/twogood/unshield for more information.
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@4036 599ab11a-0111-0410-8a50-d7a5b0953570

--- unshield/rebuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -x
-export CFLAGS="-Wall -Werror -ggdb3"
-./bootstrap &&
-./configure --prefix=/var/tmp/unshield --with-ssl &&
-make install
--- unshield/valgrind.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-libtool --mode=execute valgrind --num-callers=10 --leak-check=yes `dirname $0`/src/unshield $@
