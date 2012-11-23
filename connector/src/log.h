0a91872ad548593f02f33f25717b60ecdd530cbe
Mark Ellis <mark@mpellis.org.uk>
2012-11-23 14:23:12 +0000

    move obsolete packages out of trunk
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@4142 599ab11a-0111-0410-8a50-d7a5b0953570

--- connector/src/log.h
+++
@@ -1,5 +0,0 @@
-void
-log_to_syslog(const gchar *log_domain,
-		    GLogLevelFlags log_level,
-		    const gchar *message,
-		    gpointer user_data);
--- libsynce/rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -x
-export CFLAGS="-Wall -Werror -ggdb3"
-./autogen.sh --prefix=/var/tmp/synce --enable-dbus &&
-make install
