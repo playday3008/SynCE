playday3008/SynCE

commit 8cc5c1b36fed9359439238331e344f7c5ccd9600

When a device disconnects, remove it from dbus before doing anything else
git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@4276 599ab11a-0111-0410-8a50-d7a5b0953570

core/dccm/synce-device.c
@@ -239,6 +239,7 @@ gudev_uevent_callback(G_GNUC_UNUSED GUdevClient *client,
 
   g_debug("%s: received uevent remove for our device", G_STRFUNC);
 
+  synce_device_dbus_uninit(self);
   g_signal_emit(self, SYNCE_DEVICE_GET_CLASS(SYNCE_DEVICE(self))->signals[SYNCE_DEVICE_SIGNAL_DISCONNECTED], 0);
 
   return;
@@ -643,6 +644,41 @@ synce_device_dbus_init(SynceDevice *self)
   g_object_set (self, "object-path", obj_path, NULL);
   g_debug("%s: obj_path set to %s", G_STRFUNC, priv->obj_path);
   g_free(obj_path);
+
+  return;
+}
+
+void
+synce_device_dbus_uninit(SynceDevice *self)
+{
+  SynceDevicePrivate *priv = SYNCE_DEVICE_GET_PRIVATE (self);
+  g_return_if_fail(priv->inited && !(priv->dispose_has_run));
+
+  GError *error = NULL;
+#if !USE_GDBUS
+  DBusGConnection *system_bus = NULL;
+#endif
+
+  g_message ("%s: unregistering object path '%s'", G_STRFUNC, priv->obj_path);
+
+#if USE_GDBUS
+  g_dbus_interface_skeleton_unexport(G_DBUS_INTERFACE_SKELETON(priv->interface));
+  g_object_unref(priv->interface);
+  priv->interface = NULL;
+
+#else
+  system_bus = dbus_g_bus_get (DBUS_BUS_SYSTEM, &error);
+  if (system_bus == NULL) {
+    g_critical("Failed to connect to system bus: %s", error->message);
+    g_error_free(error);
+    g_free(obj_path);
+    return;
+  }
+
+  dbus_g_connection_unregister_g_object(system_bus, G_OBJECT(self));
+
+  dbus_g_connection_unref(system_bus);
+#endif
 
   return;
 }

core/dccm/synce-device-internal.h
@@ -94,6 +94,7 @@ void synce_device_request_connection (SynceDevice *self, DBusGMethodInvocation *
 void synce_device_change_password_flags (SynceDevice *self, SynceDevicePasswordFlags new_flag);
 void synce_device_conn_broker_done_cb (SynceConnectionBroker *broker, gpointer user_data);
 void synce_device_dbus_init(SynceDevice *self);
+void synce_device_dbus_uninit(SynceDevice *self);
 void synce_device_conn_event_cb(GObject *istream, GAsyncResult *res, gpointer user_data);
 
 G_END_DECLS

core/dccm/synce-device-legacy.c
@@ -80,6 +80,7 @@ synce_device_legacy_send_ping(gpointer data)
     g_object_get(self, "name", &name, NULL);
 
     g_warning("%s: Device %s not responded to %d pings, assume disconnected", G_STRFUNC, name, DCCM_MAX_PING_COUNT);
+    synce_device_dbus_uninit(SYNCE_DEVICE(self));
     g_signal_emit (self, SYNCE_DEVICE_GET_CLASS(SYNCE_DEVICE(self))->signals[SYNCE_DEVICE_SIGNAL_DISCONNECTED], 0);
     g_free(name);
     return FALSE;

commit 902cb964906e15065a94a32295e98f994804bfbd

Fix build with dbus-glib, broken by previous commit r4276
git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@4277 599ab11a-0111-0410-8a50-d7a5b0953570

core/dccm/synce-device.c
@@ -654,8 +654,8 @@ synce_device_dbus_uninit(SynceDevice *self)
   SynceDevicePrivate *priv = SYNCE_DEVICE_GET_PRIVATE (self);
   g_return_if_fail(priv->inited && !(priv->dispose_has_run));
 
-  GError *error = NULL;
 #if !USE_GDBUS
+  GError *error = NULL;
   DBusGConnection *system_bus = NULL;
 #endif
 
@@ -671,7 +671,6 @@ synce_device_dbus_uninit(SynceDevice *self)
   if (system_bus == NULL) {
     g_critical("Failed to connect to system bus: %s", error->message);
     g_error_free(error);
-    g_free(obj_path);
     return;
   }
 