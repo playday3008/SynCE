613ac5834d8cf8a8f3e7aa0100bf6a2c157201e2
Arndt Kaiser <madmajestro@users.noreply.github.com>
2017-02-07 03:45:30 +0000

    if a device is disconnected because it doesn't respond to pings, the io-stream must be closed to abort the pending async-read of the last ping. The aborted read also releases the last reference to the device-object
    git-svn-id: svn://svn.code.sf.net/p/synce/code/trunk@4321 599ab11a-0111-0410-8a50-d7a5b0953570

--- core/dccm/synce-device-legacy.c
+++ core/dccm/synce-device-legacy.c
@@ -79,6 +79,7 @@ synce_device_legacy_send_ping(gpointer data)
 
     g_warning("%s: Device %s not responded to %d pings, assume disconnected", G_STRFUNC, name, DCCM_MAX_PING_COUNT);
     synce_device_dbus_uninit(SYNCE_DEVICE(self));
+    g_io_stream_close(G_IO_STREAM(priv->conn), NULL, NULL);
     g_signal_emit (self, SYNCE_DEVICE_GET_CLASS(SYNCE_DEVICE(self))->signals[SYNCE_DEVICE_SIGNAL_DISCONNECTED], 0);
     g_free(name);
     return FALSE;
